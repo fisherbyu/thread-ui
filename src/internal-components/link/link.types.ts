63082b3cf600449faefa697889cfec008a33d3a7
Andrew Fisher <fisherandrew777@gmail.com>
2024-10-24 00:31:59 -0600

    Update Props

--- src/internal-components/link/link.types.ts
+++ src/internal-components/link/link.types.ts
@@ -1,5 +1,8 @@
-export type LinkProps = {
+// link.types.ts
+import type { AnchorHTMLAttributes, ReactNode } from 'react';
+
+export interface LinkProps extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> {
 	href: string;
-	children: React.ReactNode;
-	[key: string]: any; // Allow passing other props like className
-};
+	children: ReactNode;
+	prefetch?: boolean; // Next.js specific prop
+}
