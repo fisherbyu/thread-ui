f63521d39d2e34c6a5508cc59b0bb331e9932652
Andrew Fisher <fisherandrew777@gmail.com>
2024-10-24 00:01:32 -0600

    Add Container to Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 	},
 	plugins: [],
 };
