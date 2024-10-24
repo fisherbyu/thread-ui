953e02ef43b54a5a7e2297d9d6171262add29f1f
Andrew Fisher <fisherandrew777@gmail.com>
2024-10-23 22:58:31 -0600

    Import Styles into Base Export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+// Import Tailwind Styles
+import './styles.css';
+
 // Export Components
-export { InfoCard } from "./components/info-card";
+export { InfoCard } from './components/info-card';
 
 // Export Types
