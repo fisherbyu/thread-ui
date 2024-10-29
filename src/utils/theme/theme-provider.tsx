97eab481b28277de3fba5f5e935451a24bb85054
Andrew Fisher <fisherandrew777@gmail.com>
2024-10-28 22:48:23 -0600

    Create Theme Provider

--- src/utils/theme/theme-provider.tsx
+++ src/utils/theme/theme-provider.tsx
@@ -1,1 +1,19 @@
 import React, { createContext, ReactNode, useContext } from 'react';
+import { Theme } from '../../types';
+import { DEFAULT_THEME } from '.';
+
+type ThemeProviderProps = {
+	theme?: Partial<Theme>;
+	children: ReactNode;
+};
+
+const ThemeContext = createContext<Theme>(DEFAULT_THEME);
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ theme, children }) => {
+	// Merge the passed theme with the defaultTheme, prioritizing any custom values
+	const mergedTheme = { ...DEFAULT_THEME, ...theme };
+
+	return <ThemeContext.Provider value={mergedTheme}>{children}</ThemeContext.Provider>;
+};
+
+export const useTheme = () => useContext(ThemeContext);
