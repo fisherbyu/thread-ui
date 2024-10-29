00f1e076022d85321cbe73177782c9c52781666a
Andrew Fisher <fisherandrew777@gmail.com>
2024-10-28 22:17:01 -0600

    Export Shade

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,10 @@
 // Theme Types:
 export { BaseColorOptions } from './theme/colors/colors.types';
 export { ExpandedColorOptions } from './theme/colors/colors.types';
-export { LevelColorOptions } from './theme/colors/colors.types';
 export { TextColorOptions } from './theme/colors/colors.types';
 export { ColorShades } from './theme/colors/colors.types';
 export { ExpandedColorShades } from './theme/colors/colors.types';
+export { LevelColorShades } from './theme/colors/colors.types';
 
 // Element Props
 
