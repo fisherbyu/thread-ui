dd05ce0b6e168afbcb9b1ab070e8bbfd9f3e0bdc
Andrew Fisher <fisherandrew777@gmail.com>
2024-10-28 21:57:29 -0600

    Add Color Types

--- src/types/theme/colors/colors.types.ts
+++ src/types/theme/colors/colors.types.ts
@@ -1,3 +1,17 @@
-export type BaseColorLevels = 'light' & 'medium' & 'dark';
+export type BaseColorOptions = 'light' | 'medium' | 'dark';
 
-export type ExpandedColorLevels = 'extra-light' & BaseColorLevels & 'extra-dark';
+export type ExpandedColorOptions = 'extra-light' | BaseColorOptions | 'extra-dark';
+
+export type ColorShades = {
+	light: string;
+	medium: string;
+	dark: string;
+};
+
+export type ExpandedColorShades = {
+	'extra-light': string;
+	light: string;
+	medium: string;
+	dark: string;
+	'extra-dark': string;
+};
