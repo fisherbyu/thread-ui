4f43dfd4866fa00a42b4dcd95ada4951bc7227bd
Andrew Fisher <fisherandrew777@gmail.com>
2024-10-30 00:02:21 -0600

    Create Props File

---
+++ src/utils/responsive/responsive-styles.types.ts
@@ -0,0 +1,8 @@
+export type ResponsiveStylesProps = {
+	base: number | string;
+	sm?: number | string;
+	md?: number | string;
+	lg?: number | string;
+	xl?: number | string;
+	xxl?: number | string;
+};
