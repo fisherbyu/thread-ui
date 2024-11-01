36e00158da0b6fbdcc2ec922dc5abdeade8e999c
Andrew Fisher <fisherandrew777@gmail.com>
2024-11-01 15:29:33 -0600

    Update Menu

--- src/components/nav-menu/nav-menu.tsx
+++ src/components/nav-menu/nav-menu.tsx
@@ -1,3 +1,32 @@
+import { CSSProperties } from 'react';
 import { NavMenuProps } from './nav-menu.types';
+import { useTheme } from '../../utils';
 
-export const NavMenu = ({ logo, items }: NavMenuProps) => {};
+export const NavMenu = ({ logo, items }: NavMenuProps) => {
+	const theme = useTheme();
+	const style: Record<string, CSSProperties> = {
+		header: {
+			position: 'sticky',
+			top: '0px',
+			width: '100%',
+			display: 'flex',
+			alignItems: 'center',
+			height: '80px',
+			backdropFilter: 'blur(24px)',
+			borderBottomWidth: '1px',
+			borderBottomColor: theme.colors.structure.light,
+			zIndex: '40',
+			backgroundColor: theme.colors.background.light,
+		},
+	};
+
+	return (
+		<>
+			<header id="site-menu" style={style.header}>
+				<nav className=" container relative mx-auto w-full flex gap-x-5 justify-between lg:justify-start items-center"></nav>
+			</header>
+		</>
+	);
+};
+
+// className=" dark:bg-gray-950/80 backdrop-filter backdrop-blur-xl"
--- src/components/nav-menu/nav-menu.types.ts
+++ src/components/nav-menu/nav-menu.types.ts
@@ -1,10 +1,16 @@
 import { NavIconItemProps } from './items/nav-icon-item';
 import { NavItemProps } from './items/nav-item';
 
-type NavLogo = NavIconItemProps;
-type NavItem = NavItemProps;
+type NavLogo = {
+	href: string;
+	logo: React.ReactNode;
+};
+type NavItem = {
+	href: string;
+	title: string;
+};
 
 export type NavMenuProps = {
 	logo?: NavLogo;
-	items: [NavItem];
+	items: NavItem[];
 };
