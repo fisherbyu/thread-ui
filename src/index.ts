d4587367938ff701db70c9e117b2565a72b71957
Andrew Fisher <fisherandrew777@gmail.com>
2024-11-01 15:29:10 -0600

    Change Export Structure

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
 import './styles.css';
 
 // Export Components
-export { InfoCard } from './components/info-card';
-export { NavMenu } from './components/nav-menu';
+export * from './components/info-card';
+export * from './components/nav-menu';
 
 // Export Types
 export * from './types';
