87ce0a572d1cab583893524cb8336b47eb6c8750
Andrew Fisher <fisherandrew777@gmail.com>
2024-11-01 17:53:54 -0600

    Save Changes

--- src/components/nav-menu/nav-menu.tsx
+++ src/components/nav-menu/nav-menu.tsx
@@ -2,8 +2,9 @@ import { CSSProperties } from 'react';
 import { NavMenuProps } from './nav-menu.types';
 import { useResponsiveStyles, useTheme } from '../../utils';
 import { NavIconItem } from './items/nav-icon-item';
-import { NavItemProps } from './items/nav-item';
+import { NavItem, NavItemProps } from './items/nav-item';
 import { NavDropdownItemProps } from './items/nav-drop-down-item/nav-drop-down-item.types';
+import { NavDropdownItem } from './items/nav-drop-down-item';
 
 export const NavMenu = ({ logo, items }: NavMenuProps) => {
 	const theme = useTheme();
@@ -36,19 +37,31 @@ export const NavMenu = ({ logo, items }: NavMenuProps) => {
 		},
 	};
 
-	const _renderNavItem = ({ href, title }: NavItemProps) => {};
-	const _renderNavDropdown = ({ title, items }: NavDropdownItemProps) => {};
+	const _renderNavItem = ({ href, title }: NavItemProps) => {
+		return <NavItem href={href} title={title} />;
+	};
+	const _renderNavDropdown = ({ title, items }: NavDropdownItemProps) => {
+		return <NavDropdownItem title={title} items={items} />;
+	};
+
+	const _renderItem = (item: NavItemProps | NavDropdownItemProps) => {
+		if ('href' in item) {
+			// item is of type NavItem
+			return _renderNavItem(item);
+		} else {
+			// item is of type NavDropdownItem
+			return _renderNavDropdown(item);
+		}
+	};
 
 	return (
 		<>
 			<header id="site-menu" style={style.header}>
 				<nav style={style.nav}>
 					{logo && <NavIconItem href={logo.href} logo={logo.logo} />}
-					<span>x</span>
+					{items.map((item) => _renderItem(item))}
 				</nav>
 			</header>
 		</>
 	);
 };
-
-// className=" dark:bg-gray-950/80 backdrop-filter backdrop-blur-xl"
