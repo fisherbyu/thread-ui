2743fbbe468b18c41a4a44d8ceba3ba1011c9a60
Andrew Fisher <fisherandrew777@gmail.com>
2024-11-01 00:02:55 -0600

    Remove Previous NavItem

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import './styles.css';
 
 // Export Components
 export { InfoCard } from './components/info-card';
-export * from './components/nav-menu/items/nav-item/nav-item';
 
 // Export Types
 export * from './types';
