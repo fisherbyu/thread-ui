e0a95e633e26351ea42d8d7a8206269ae0433b5d
Andrew Fisher <fisherandrew777@gmail.com>
2024-11-02 02:55:18 -0600

    Correct Props

--- src/components/nav-menu/items/nav-drop-down-item/nav-drop-down-item.types.ts
+++ src/components/nav-menu/items/nav-drop-down-item/nav-drop-down-item.types.ts
@@ -1,6 +1,6 @@
-import { NavIconItemProps } from '../nav-icon-item';
+import { NavItemProps } from '../nav-item';
 
 export type NavDropdownItemProps = {
 	title: string;
-	items: NavIconItemProps[];
+	items: NavItemProps[];
 };
