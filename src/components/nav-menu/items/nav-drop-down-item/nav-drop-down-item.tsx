28349ca10e0118e5f7b81220bc030ee801be7ad9
Andrew Fisher <fisherandrew777@gmail.com>
2024-11-05 00:24:01 -0700

    Dropdown Work

--- src/components/nav-menu/items/nav-drop-down-item/nav-drop-down-item.tsx
+++ src/components/nav-menu/items/nav-drop-down-item/nav-drop-down-item.tsx
@@ -1,32 +1,67 @@
-import { useState } from 'react';
+import { CSSProperties, useState } from 'react';
 import { BaseItem } from '../base-item';
 import { NavDropdownItemProps } from './nav-drop-down-item.types';
+import { relative } from 'path';
+import { NavMenuSpacing } from '../../nav-menu-spacing';
 
 export const NavDropdownItem = ({ title, items }: NavDropdownItemProps) => {
 	const [isHovered, setIsHovered] = useState(false);
+
+	const styles: Record<string, CSSProperties> = {
+		title: {
+			display: 'flex',
+			flexDirection: 'row',
+			alignItems: 'center',
+			justifyContent: 'space-between',
+			gap: '2px',
+		},
+
+		anchor: {
+			width: '100%',
+			height: '0px',
+		},
+
+		dropdownContent: {
+			height: 'fit-content',
+			width: `calc(100% + ${NavMenuSpacing.paddingX * 1.75 * 2}px)`,
+			position: 'relative',
+			left: `-${NavMenuSpacing.paddingX * 1.75}px`,
+			top: '-9px',
+			display: 'flex',
+			flexDirection: 'column',
+			gap: '24px',
+		},
+
+		spacer: { height: '40px', width: '100%' },
+
+		dropdownItems: {
+			position: 'relative',
+			// width: `calc(100% + ${NavMenuSpacing.paddingX}px`,
+			width: '100%',
+			height: 'fit-content',
+			left: `-${NavMenuSpacing.paddingX * 0.5}px`,
+			padding: `${NavMenuSpacing.paddingY}px ${NavMenuSpacing.paddingX}px`,
+			display: 'flex',
+			flexDirection: 'column',
+		},
+	};
 	return (
-		<BaseItem href="href" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-			{title}
-			<div
-				className=" border"
-				id="dropdownContent"
-				style={{
-					width: 'calc(100%)',
-					height: 'fit-content',
-					position: 'relative',
-					left: '-33px',
-					top: '-9px',
-					display: 'flex',
-					flexDirection: 'column',
-					gap: '22px',
-				}}
-			>
-				<div id="spacer" style={{ height: '22px', width: '100%' }}></div>
-				<div id="DropdownItems" style={{ height: '20px', width: '100%' }}>
-					{items.map((item) => (
-						<BaseItem href={item.href}>{item.title}</BaseItem>
-					))}
+		<BaseItem href="#" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+			<div style={styles.anchor}>
+				<div id="dropdownContent" className="border" style={styles.dropdownContent}>
+					<div id="spacer" style={styles.spacer} />
+					<div style={styles.anchor}>
+						<div style={styles.dropdownItems} className="border">
+							{/* {items.map((item) => (
+								<BaseItem href={item.href}>{item.title}</BaseItem>
+							))} */}
+						</div>
+					</div>
 				</div>
+			</div>
+			<div style={styles.title}>
+				<span>{title}</span>
+				<span>X</span>
 			</div>
 		</BaseItem>
 	);
