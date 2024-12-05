8f4cda648ee907dcbf6681ae869bef5d936d6e0b
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-05 16:30:32 -0700

    Add applied themes

--- src/types/theme/theme.types.ts
+++ src/types/theme/theme.types.ts
@@ -1,9 +1,15 @@
 import { ThemeBorders } from './borders/themeBorders.types';
-import { ThemeColors } from './colors/themeColors.types';
+import { AppliedThemeColors, ThemeColors } from './colors/themeColors.types';
 import { ThemeSizes } from './size/themeSizes.types';
 
 export type Theme = {
 	borders: ThemeBorders;
 	colors: ThemeColors;
+	sizes: ThemeSizes;
+};
+
+export type AppliedTheme = {
+	borders: ThemeBorders;
+	colors: AppliedThemeColors;
 	sizes: ThemeSizes;
 };
