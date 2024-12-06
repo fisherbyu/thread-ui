f6c6d6b75f526b9ec4b3f8648b74371438b595cc
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-06 00:13:26 -0700

    Make exports explicit

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,29 +1,24 @@
 // THEME TYPES
-export * from './theme/theme.types';
-// export { Theme } from './theme/theme.types';
-// export { AppliedTheme } from './theme/theme.types';
+export { Theme } from './theme/theme.types';
+export { AppliedTheme } from './theme/theme.types';
+export { ColorMode } from './theme/theme.types';
 
 // Color
-export * from './theme/colors/colors.types';
-export * from './theme/colors/themeColors.types';
-// export { BaseColorOptions } from './theme/colors/colors.types';
-// export { ExpandedColorOptions } from './theme/colors/colors.types';
-// export { ColorShades } from './theme/colors/colors.types';
-// export { ExpandedColorShades } from './theme/colors/colors.types';
-// export { ModeColorShades } from './theme/colors/colors.types';
-// export { TextColorShades } from './theme/colors/colors.types';
-// export { ThemeColors } from './theme/colors/themeColors.types';
-// export { AppliedThemeColors } from './theme/colors/themeColors.types';
+export { BaseColorOptions } from './theme/colors/colors.types';
+export { ExpandedColorOptions } from './theme/colors/colors.types';
+export { ColorShades } from './theme/colors/colors.types';
+export { ExpandedColorShades } from './theme/colors/colors.types';
+export { ModeColorShades } from './theme/colors/colors.types';
+export { TextColorShades } from './theme/colors/colors.types';
+export { ThemeColors } from './theme/colors/themeColors.types';
+export { AppliedThemeColors } from './theme/colors/themeColors.types';
 
 // Size
-export * from './theme/size/themeSizes.types';
-// export { ThemeSizes } from './theme/size/themeSizes.types';
+export { ThemeSizes } from './theme/size/themeSizes.types';
 
 // Border
-export * from './theme/borders/borders.types';
-// export { BorderSizes } from './theme/borders/borders.types';
-// export { ThemeBorders } from './theme/borders/themeBorders.types';
+export { BorderSizes } from './theme/borders/borders.types';
+export { ThemeBorders } from './theme/borders/themeBorders.types';
 
 // IMAGE TYPES
-export * from './image/image.types';
-// export { ImageProps } from './image/image.types';
+export { ImageProps } from './image/image.types';
