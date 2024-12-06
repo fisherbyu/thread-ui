2c7f279148a9ddea47b66f94d49784e2d5188a8b
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-06 00:55:29 -0700

    Use color options

--- src/components/nav-menu/items/nav-icon-item/nav-icon-item.types.ts
+++ src/components/nav-menu/items/nav-icon-item/nav-icon-item.types.ts
@@ -1,6 +1,8 @@
 import { ReactNode } from 'react';
+import { ColorOptions } from '../../../../types';
 
 export type NavIconItemProps = {
 	href: string;
 	logo: ReactNode;
+	color?: ColorOptions;
 };
