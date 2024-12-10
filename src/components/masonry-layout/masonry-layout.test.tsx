df2553ba9a92f5450fd37edcbed47e4baa40c012
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-09 22:52:39 -0700

    Configure for TSX

--- src/components/masonry-layout/masonry-layout.test.tsx
+++ src/components/masonry-layout/masonry-layout.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { MasonryLayout } from './masonry-layout';
-import { describe, it, expect, afterEach, test } from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
+import '@testing-library/jest-dom';
 
 const items = [<div>Hello</div>, <div>Goodbye</div>];
 
@@ -8,6 +9,6 @@ describe('Masonry Layout', () => {
 	test('Render ML', () => {
 		render(<MasonryLayout components={items} />);
 
-		expect(screen.getAllByTestId('masonry'));
+		expect(screen.getAllByTestId('masonry')).toBeInTheDocument;
 	});
 });
