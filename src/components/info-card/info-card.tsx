ad795a7bf0bfff0974dfb1eaefcd4e599773ab2e
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-12 11:58:40 -0700

    Apply props

--- src/components/info-card/info-card.tsx
+++ src/components/info-card/info-card.tsx
@@ -1,33 +1,23 @@
 import { InfoCardProps } from './info-card.types';
 
 export const InfoCard = ({ title, url, icon, img }: InfoCardProps) => {
-	const page = {
-		id: 1,
-		title: 'string',
-		url: '#',
-		icon: {
-			type: 'emoji',
-			content: '🤘',
-		},
-		img: 'https://images.unsplash.com/photo-1685397166786-b43234186139?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb',
-	};
 	return (
 		<div className="mx-auto rounded-lg border bg-card text-card-foreground shadow-sm my-3 overflow-hidden max-h-60 w-[391px] h-[241px]">
-			<a href={page.url}>
+			<a href={url}>
 				<span className=" cursor-pointer">
 					<div className="overflow-hidden w-full h-5/6">
-						<img className="h-auto min-h-full w-auto min-w-full" src={page.img} alt="recipe cover" />
+						<img className="h-auto min-h-full w-auto min-w-full" src={img} alt="recipe cover" />
 					</div>
 					<div className="p-2 h-1/6">
 						<span className="flex justify-start items-center">
-							{page.icon.type === 'emoji' ? (
+							{icon.type === 'emoji' ? (
 								<span className=" pr-1" role="img">
-									{JSON.parse(`"${page.icon.content}"`)}
+									{JSON.parse(`"${icon.content}"`)}
 								</span>
 							) : (
-								<img className="pr-1" height={23} width={23} src={page.icon.content} alt="Recipe Logo" />
+								<img className="pr-1" height={23} width={23} src={icon.content} alt="Recipe Logo" />
 							)}
-							<p className=" text-sm">{page.title}</p>
+							<p className=" text-sm">{title}</p>
 						</span>
 					</div>
 				</span>
