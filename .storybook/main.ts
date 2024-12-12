23b20a5fed9822caf548fa45f361bbde7a7d77f2
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-12 13:38:25 -0700

    Uninstall Storybook Visual testing

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
 	stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-	addons: ['@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+	addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
 	framework: {
 		name: '@storybook/react-webpack5',
 		options: {},
