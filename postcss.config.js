88356a9f06204ae4dd49fd56b94590b700e16454
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-11 23:49:05 -0700

    Prefix outputed classes with 'thread-'

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,20 @@
 module.exports = {
     plugins: {
         tailwindcss: {},
+        'postcss-prefix-selector': {
+            prefix: 'thread-',
+            transform(prefix, selector) {
+                // Don't prefix already prefixed selectors
+                if (selector.includes(prefix)) {
+                    return selector;
+                }
+                // Don't prefix @keyframes
+                if (selector.includes('@keyframes')) {
+                    return selector;
+                }
+                return `${prefix}${selector}`;
+            }
+        },
         autoprefixer: {},
     }
 }
