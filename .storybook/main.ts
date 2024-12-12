cd37e36390ce5e628cb0c7bab2d0e71699c43ff9
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-12 11:26:00 -0700

    Configure Storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,35 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+	stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	addons: ['@storybook/addon-essentials', '@chromatic-com/storybook', '@storybook/addon-interactions'],
+	framework: {
+		name: '@storybook/react-webpack5',
+		options: {},
+	},
+	typescript: {
+		check: true,
+		reactDocgen: 'react-docgen-typescript',
+	},
+	webpackFinal: async (config) => {
+		if (!config.module?.rules) return config;
+
+		// Add TypeScript handling
+		config.module.rules.push({
+			test: /\.(ts|tsx)$/,
+			exclude: /node_modules/,
+			use: [
+				{
+					loader: 'ts-loader',
+					options: {
+						transpileOnly: true,
+					},
+				},
+			],
+		});
+
+		return config;
+	},
+};
+
+export default config;
