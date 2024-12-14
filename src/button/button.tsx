38052b22c2cb3690d322cb5aefa8183e43039b6a
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-14 12:34:10 -0700

    Allow fo color mapping/changes

--- src/button/button.tsx
+++ src/button/button.tsx
@@ -1,9 +1,92 @@
+'use client';
+import { CSSProperties, useState } from 'react';
 import { useTheme } from '../utils';
 import { ButtonProps } from './button.types';
 
-export const Button = ({ content, fullWidth, color }: ButtonProps) => {
+export const Button = ({ content, fullWidth, color = 'primary' }: ButtonProps) => {
 	const theme = useTheme();
 	const width = fullWidth ? 'w-full' : 'w-fit';
+	const [isHovered, setIsHovered] = useState(false);
 
-	return <div className={`p-2 ${fullWidth}`}>{content}</div>;
+	const getColorValue = (colorType: ButtonProps['color']): { dark: string; light: string } => {
+		switch (colorType) {
+			case 'primary':
+				return {
+					dark: theme.colors.primary.medium,
+					light: theme.colors.primary.light,
+				};
+			case 'secondary':
+				return {
+					dark: theme.colors.secondary.medium,
+					light: theme.colors.secondary.light,
+				};
+			case 'tertiary':
+				return {
+					dark: theme.colors.tertiary.medium,
+					light: theme.colors.tertiary.light,
+				};
+			case 'black':
+				return {
+					dark: theme.colors.black,
+					light: theme.colors.white,
+				};
+			case 'grey':
+				return {
+					dark: theme.colors.gray.dark,
+					light: theme.colors.gray.light,
+				};
+			case 'success':
+				return {
+					dark: theme.colors.success.medium,
+					light: theme.colors.success.light,
+				};
+			case 'error':
+				return {
+					dark: theme.colors.error.medium,
+					light: theme.colors.error.light,
+				};
+			case 'info':
+				return {
+					dark: theme.colors.info.medium,
+					light: theme.colors.primary.light,
+				};
+			default:
+				return {
+					dark: theme.colors.primary.medium,
+					light: theme.colors.primary.medium,
+				};
+		}
+	};
+
+	const colors = getColorValue(color);
+
+	const buttonStyles: CSSProperties = {
+		padding: `${theme.space * 3}px`,
+		width: fullWidth ? '100%' : 'fit-content',
+		transition: 'background-color 0.2s ease',
+		border: `${theme.borders.size.md}px ${colors.dark} solid`,
+		borderRadius: `${theme.borders.radius.md}px`,
+		color: theme.colors.white,
+		backgroundColor: colors.dark,
+		margin: 'auto',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+	};
+
+	const hoverButton: CSSProperties = {
+		...buttonStyles,
+		backgroundColor: theme.colors.white,
+		color: colors.dark,
+	};
+
+	return (
+		<div
+			style={isHovered ? hoverButton : buttonStyles}
+			onMouseEnter={() => setIsHovered(true)}
+			onMouseLeave={() => setIsHovered(false)}
+		>
+			{content}
+		</div>
+	);
 };
