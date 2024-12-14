a4107944390eca2c709a697e662570a77e8c1771
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-14 12:43:48 -0700

    Add onClick Prop

--- src/button/button.types.ts
+++ src/button/button.types.ts
@@ -2,4 +2,5 @@ export type ButtonProps = {
 	content: string;
 	fullWidth?: boolean;
 	color: 'primary' | 'secondary' | 'tertiary' | 'black' | 'grey' | 'success' | 'error' | 'info';
+	onClick?: () => void;
 };
