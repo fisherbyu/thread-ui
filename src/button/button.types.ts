ae13fbf7a77384b5f9e45679fce8f639caca22f1
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-14 12:48:56 -0700

    Add type prop, make color optional

--- src/button/button.types.ts
+++ src/button/button.types.ts
@@ -1,6 +1,7 @@
 export type ButtonProps = {
 	content: string;
 	fullWidth?: boolean;
-	color: 'primary' | 'secondary' | 'tertiary' | 'black' | 'grey' | 'success' | 'error' | 'info';
+	color?: 'primary' | 'secondary' | 'tertiary' | 'black' | 'grey' | 'success' | 'error' | 'info';
+	type?: 'button' | 'submit' | 'reset';
 	onClick?: () => void;
 };
