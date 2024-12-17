1da3e7d1439fde6ad3c1b1eabd6bde1146951af7
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-17 03:12:46 -0700

    Remove Image

--- src/internal-components/image/image.types.ts
+++
@@ -1,4 +0,0 @@
-import { ImgHTMLAttributes } from 'react';
-import { ImageProps as DefaultImageProps } from '../../types';
-
-export interface ImageProps extends DefaultImageProps {}
--- src/internal-components/image/index.ts
+++
@@ -1,2 +0,0 @@
-export { ImageProps } from './image.types';
-export { Image } from './image';
