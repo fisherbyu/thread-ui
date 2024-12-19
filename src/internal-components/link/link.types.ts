da8548eeaf6cb6ae683cfb594a5245d547f57ea6
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-19 01:48:19 -0700

    Remove Old Link Component

--- src/internal-components/link/link.types.ts
+++
@@ -1,7 +0,0 @@
-import type { AnchorHTMLAttributes, ReactNode } from 'react';
-
-export interface LinkProps extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href'> {
-	href: string;
-	children: ReactNode;
-	prefetch?: boolean; // Next.js specific prop
-}
