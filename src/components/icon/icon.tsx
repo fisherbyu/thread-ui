422bbed47ffcd5a1d057846dcd12040a3b959ac2
Andrew Fisher <fisherandrew777@gmail.com>
2024-12-23 23:49:34 -0800

    Correct usage

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import * as PhosphorIcons from '@phosphor-icons/react';
 import { IconProps } from './icon.types';
+import { IconWeight } from '@phosphor-icons/react';
 import { getUtilityColorValue, useTheme } from '../../utils';
-export const Icon: React.FC<IconProps> = ({ name, color, size = 24, filled = false }) => {
-	const theme = useTheme();
 
-	const IconComponent = PhosphorIcons[name] as React.ComponentType<{ color?: string; size?: number; style?: React.CSSProperties }>;
+export const Icon: React.FC<IconProps> = ({ name, color, size = 24, weight = 'regular', filled = false }) => {
+	const theme = useTheme();
+	const IconComponent = PhosphorIcons[name] as React.ComponentType<{
+		color?: string;
+		size?: number;
+		weight?: IconWeight;
+	}>;
+
 	const colorValue = getUtilityColorValue(color);
-	return <IconComponent color={colorValue} size={size} style={{ fill: filled ? color : 'none' }} />;
+
+	return <IconComponent color={colorValue} size={size} weight={filled ? 'fill' : weight} />;
 };
