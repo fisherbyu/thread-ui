982f06b5ae0a094dd79dcaf5097a1c47d5cdeec3
Andrew Fisher <fisherandrew777@gmail.com>
2025-02-19 16:29:25 -0700

    Remove utility

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,56 +7,6 @@ export const Button = ({ children, fullWidth, color = 'primary', onClick, type =
 	const theme = useTheme();
 	const [isHovered, setIsHovered] = useState(false);
 
-	// const getColorValue = (colorType: ButtonProps['color']): { dark: string; light: string } => {
-	// 	switch (colorType) {
-	// 		case 'primary':
-	// 			return {
-	// 				dark: theme.colors.primary.medium,
-	// 				light: theme.colors.primary.light,
-	// 			};
-	// 		case 'secondary':
-	// 			return {
-	// 				dark: theme.colors.secondary.medium,
-	// 				light: theme.colors.secondary.light,
-	// 			};
-	// 		case 'tertiary':
-	// 			return {
-	// 				dark: theme.colors.tertiary.medium,
-	// 				light: theme.colors.tertiary.light,
-	// 			};
-	// 		case 'black':
-	// 			return {
-	// 				dark: theme.colors.black,
-	// 				light: theme.colors.white,
-	// 			};
-	// 		case 'grey':
-	// 			return {
-	// 				dark: theme.colors.gray.medium,
-	// 				light: theme.colors.gray.light,
-	// 			};
-	// 		case 'success':
-	// 			return {
-	// 				dark: theme.colors.success.medium,
-	// 				light: theme.colors.success.light,
-	// 			};
-	// 		case 'error':
-	// 			return {
-	// 				dark: theme.colors.error.medium,
-	// 				light: theme.colors.error.light,
-	// 			};
-	// 		case 'info':
-	// 			return {
-	// 				dark: theme.colors.info.medium,
-	// 				light: theme.colors.primary.light,
-	// 			};
-	// 		default:
-	// 			return {
-	// 				dark: theme.colors.primary.medium,
-	// 				light: theme.colors.primary.medium,
-	// 			};
-	// 	}
-	// };
-
 	const colorValue = getUtilityColorValue(color);
 
 	const buttonStyles: CSSProperties = {
