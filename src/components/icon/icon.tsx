6b34f3f88a189d2de2f0bf57c0de5d58742f97e4
Andrew Fisher <fisherandrew777@gmail.com>
2025-02-20 22:11:04 -0700

    Add JsDocs, remove unecessary theme

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -5,8 +5,16 @@ import { IconProps } from './icon.types';
 import { IconWeight } from '@phosphor-icons/react';
 import { getUtilityColorValue, useTheme } from '../../utils';
 
+/**
+ * Standard Icon Component from Phosphor
+ * @param name Icon Name from Phosphor Icons
+ * @param color Icon Color
+ * @param size Icon Size [8 | 12 | 16 | 24 | 32 | 48 | 64]
+ * @param weight Icon Weight ['light' | 'regular' | 'bold']
+ * @param filled Icon Fill
+ * @returns ReactNode Icon
+ */
 export const Icon: React.FC<IconProps> = ({ name, color, size = 24, weight = 'regular', filled = false }) => {
-	const { theme } = useTheme();
 	const IconComponent = PhosphorIcons[name] as React.ComponentType<{
 		color?: string;
 		size?: number;
