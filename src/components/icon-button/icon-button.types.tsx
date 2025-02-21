68ed7b6f8b91473bb9e9c163163b91c3101756fe
Andrew Fisher <fisherandrew777@gmail.com>
2025-02-20 22:15:28 -0700

    Use Utility Sizes

--- src/components/icon-button/icon-button.types.tsx
+++ src/components/icon-button/icon-button.types.tsx
@@ -1,9 +1,10 @@
 import { ReactNode } from 'react';
 import { RootButtonProps } from '../button/base-button/base-button.types';
 import { IconNames } from '../icon/icon.types';
+import { UtilityColorOptions } from '../../types';
 
 export type IconButtonProps = RootButtonProps & {
 	name: IconNames;
-	size: 'sm' | 'md' | 'lg';
+	size: UtilityColorOptions;
 	children?: ReactNode;
 };
