e96091e66e7c2c20adb91d5b8a848f374ee1dbe9
Andrew Fisher <fisherandrew777@gmail.com>
2025-02-20 22:39:36 -0700

    Create IconButton Stories

---
+++ src/components/icon-button/icon-button.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { IconButton } from './icon-button';
+import * as PhosphorIcons from '@phosphor-icons/react';
+
+const iconNames = Object.keys(PhosphorIcons).filter(
+	(key) =>
+		typeof PhosphorIcons[key as keyof typeof PhosphorIcons] === 'function' &&
+		!['Icon', 'IconBase', 'IconContext', 'IconWeight'].includes(key)
+);
+
+const meta: Meta<typeof IconButton> = {
+	title: 'Components/IconButton',
+	component: IconButton,
+	tags: ['autodocs'],
+	parameters: {
+		layout: 'centered',
+	},
+	argTypes: {
+		name: {
+			control: 'select',
+			options: iconNames,
+			description: 'Icon name from Phosphor Icons library',
+		},
+		size: {
+			control: 'radio',
+			options: ['sm', 'md', 'lg'],
+			description: 'Button size',
+		},
+		color: {
+			control: 'select',
+			options: ['primary', 'secondary', 'success', 'warning', 'danger'],
+			description: 'Button color theme',
+		},
+		fullWidth: {
+			control: 'boolean',
+			description: 'Whether button should take full width',
+		},
+		disabled: {
+			control: 'boolean',
+			description: 'Disabled state',
+		},
+		type: {
+			control: 'radio',
+			options: ['button', 'submit', 'reset'],
+			description: 'HTML button type',
+		},
+		margin: {
+			control: 'text',
+			description: 'CSS margin value',
+		},
+		onClick: {
+			action: 'clicked',
+			description: 'Click handler',
+		},
+	},
+} satisfies Meta<typeof IconButton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+	args: {
+		name: 'Heart',
+		size: 'md',
+		color: 'primary',
+		type: 'button',
+		children: 'Love',
+	},
+};
