4c8cf16a316caa1654d3a1a5bac67c695353970d
Andrew Fisher <fisherandrew777@gmail.com>
2025-02-20 22:18:11 -0700

    Create Size Handler util

---
+++ src/utils/get-utility-size/get-utility-size-value.ts
@@ -0,0 +1,16 @@
+'use client';
+
+import { UtilitySizes } from '../../types';
+
+export const getUtilitySizeValues = (size: UtilitySizes): 12 | 24 | 64 => {
+	switch (size) {
+		case 'sm':
+			return 12;
+		case 'md':
+			return 24;
+		case 'lg':
+			return 64;
+		default:
+			return 24;
+	}
+};
