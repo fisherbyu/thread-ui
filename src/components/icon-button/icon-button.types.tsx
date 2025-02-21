3ee25e1c69a1e950d1de5a10d703c9f137f0c476
Andrew Fisher <fisherandrew777@gmail.com>
2025-02-20 22:00:21 -0700

    Create IconButtonProps

--- src/components/icon-button/icon-button.types.tsx
+++ src/components/icon-button/icon-button.types.tsx
@@ -1,3 +1,9 @@
-import { ButtonProps } from '../button';
+import { ReactNode } from 'react';
+import { RootButtonProps } from '../button/base-button/base-button.types';
+import { IconNames } from '../icon/icon.types';
 
-export type IconButtonProps = ButtonProps;
+export type IconButtonProps = RootButtonProps & {
+	name: IconNames;
+	size: 'sm' | 'md' | 'lg';
+	children?: ReactNode;
+};
