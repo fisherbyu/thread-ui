957561072795ff4875e31c4bebfaa6d53e52f119
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-01 00:26:15 -0600

    Correctly render images and contents, use all props

--- src/components/image-panel/image-panel.tsx
+++ src/components/image-panel/image-panel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+'use client';
 import { ImagePanelProps } from './image-panel.types';
 import { renderImage } from '../../internal-components';
 import { Subtitle, Text, Title } from '../typography';
@@ -6,47 +6,53 @@ import { CSSProperties } from 'react';
 import { useResponsiveStyles } from '../../utils';
 import { ImageProps } from '../../types';
 
-// const renderPanelImage = (image: ImageProps, smImage?: ImageProps) => {
-// 	const styles: Record<string, CSSProperties> = {
-// 		smImage: {
-// 			position: 'relative',
-// 			display: useResponsiveStyles({ sm: 'none', lg: 'block' }),
-// 			borderRadius: '0.25rem',
-// 		},
-// 		standardImage: {
-// 			width: '100%',
-// 			height: '100%',
-// 			borderRadius: '0.25rem',
-// 		},
-// 		image: {
-// 			display: useResponsiveStyles({ sm: 'block', lg: 'none' }),
-// 			marginLeft: useResponsiveStyles({ sm: '0', md: 'auto' }),
-// 			marginRight: useResponsiveStyles({ sm: '0', md: 'auto' }),
-// 		},
-// 	};
-// 	const lgImage = renderImage(image);
-// 	const smallImage = renderImage(image);
+const renderPanelImage = (image: ImageProps, smImage?: ImageProps) => {
+	const styles: Record<string, CSSProperties> = {
+		smImage: {
+			position: 'relative',
+			display: useResponsiveStyles({ sm: 'block', lg: 'none' }),
+			borderRadius: '0.25rem',
+		},
+		standardImg: {
+			width: '100%',
+			height: '100%',
+			borderRadius: '0.25rem',
+		},
+		image: {
+			display: useResponsiveStyles({ sm: 'none', lg: 'block' }),
+			marginLeft: useResponsiveStyles({ sm: '0', md: 'auto' }),
+			marginRight: useResponsiveStyles({ sm: '0', md: 'auto' }),
+		},
+	};
 
-//     const minImage = React.cloneElement(lgImage, {
-//         style: {
-//             ...lgImage.props.style,
+	if (smImage) {
+		return (
+			<>
+				{renderImage(smImage, { ...styles.smImage, ...styles.standardImg })}
+				{renderImage(image, { ...styles.image, ...styles.standardImg })}
+			</>
+		);
+	} else {
+		return renderImage(image);
+	}
+};
 
-//         }
-//     })
-
-// 	return (
-// 		<>
-// 			{renderImage(image)}
-// 			{smImage && renderImage(smImage)}
-// 		</>
-// 	);
-// };
-
-export const ImagePanel = ({ title, subtitle, contents, image, smImage, contentBelow, contentLeft }: ImagePanelProps) => {
+export const ImagePanel = ({
+	title,
+	subtitle,
+	contents,
+	image,
+	smImage,
+	contentBelow = false,
+	contentLeft = false,
+}: ImagePanelProps) => {
 	const styles: Record<string, CSSProperties> = {
 		container: {
 			display: 'flex',
-			flexDirection: useResponsiveStyles({ sm: 'column-reverse', lg: 'row' }),
+			flexDirection: useResponsiveStyles({
+				sm: contentBelow ? 'column' : 'column-reverse',
+				lg: contentLeft ? 'row-reverse' : 'row',
+			}),
 			gap: useResponsiveStyles({ sm: '12px', lg: '20px' }),
 			justifyContent: 'center',
 			alignItems: 'stretch',
@@ -76,15 +82,14 @@ export const ImagePanel = ({ title, subtitle, contents, image, smImage, contentB
 	};
 	return (
 		<div style={styles.container}>
-			<div style={styles.imageBlock}>{renderImage(image)} </div>
+			<div style={styles.imageBlock}>{renderPanelImage(image, smImage)}</div>
 			<div style={styles.textBlock}>
 				<div>
-					<Title inline>I'm Andrew!</Title>
-					<Text>
-						I'm a Master of Information Systems Managment Student at BYU interested in software development. I love serving
-						and getting to know people and using technology to create powerful solutions. I love music and music history
-						and am an avid cook and baker. Thanks for visiting my site!
-					</Text>
+					<Title inline>
+						{title}
+						{subtitle && <Subtitle>{subtitle}</Subtitle>}
+					</Title>
+					{contents?.map((txt) => <Text>{txt}</Text>)}
 				</div>
 			</div>
 		</div>
