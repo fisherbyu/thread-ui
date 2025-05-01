36277315b0cd52b3a1d0b0033bfdb3c4ce9ad6e8
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-30 19:37:00 -0600

    Make Props more robust

--- src/types/image/image.types.ts
+++ src/types/image/image.types.ts
@@ -3,6 +3,8 @@ import { ReactNode } from 'react';
 export type HtmlImageProps = {
 	src: string;
 	alt: string;
+	width?: number | string;
+	height?: number | string;
 };
 
 export type ImageProps = HtmlImageProps | ReactNode;
