054637df5273d2138ce34ba54dc97b75f920d3ad
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-01 02:04:50 -0600

    Remove Tailwind

--- src/internal-components/column-skeleton/column-skeleton.tsx
+++ src/internal-components/column-skeleton/column-skeleton.tsx
@@ -1,33 +1,17 @@
+'use client';
+import { CSSProperties } from 'react';
 import { ColumnSkeletonProps } from './column-skeleton.types';
+import { useResponsiveStyles } from '../../utils';
 
 export const ColumnSkeleton = ({ mdcol, lgcol, children }: ColumnSkeletonProps) => {
-	const mdColMapping = {
-		1: 'md:thread-grid-cols-1',
-		2: 'md:thread-grid-cols-2',
-		3: 'md:thread-grid-cols-3',
-		4: 'md:thread-grid-cols-4',
-		5: 'md:thread-grid-cols-5',
-		6: 'md:thread-grid-cols-6',
+	const getColumnCSS = (colNum: number) => {
+		return `repeat(${colNum}, minmax(0, 1fr))`;
 	};
-	const lgColMapping = {
-		1: 'lg:thread-grid-cols-1',
-		2: 'lg:thread-grid-cols-2',
-		3: 'lg:thread-grid-cols-3',
-		4: 'lg:thread-grid-cols-4',
-		5: 'lg:thread-grid-cols-5',
-		6: 'lg:thread-grid-cols-6',
-		7: 'lg:thread-grid-cols-7',
-		8: 'lg:thread-grid-cols-8',
-		9: 'lg:thread-grid-cols-9',
-		10: 'lg:thread-grid-cols-10',
-		11: 'lg:thread-grid-cols-11',
-		12: 'lg:thread-grid-cols-12',
+
+	const styles: CSSProperties = {
+		display: 'grid',
+		gridTemplateColumns: useResponsiveStyles({ sm: getColumnCSS(1), md: getColumnCSS(mdcol), lg: getColumnCSS(lgcol) }),
+		gap: useResponsiveStyles({ sm: '3rem', lg: '1.5rem' }),
 	};
-	return (
-		<div
-			className={`thread-grid thread-grid-cols-1 md:thread-grid-cols-${mdcol} lg:thread-grid-cols-${lgcol} thread-gap-12 lg:thread-gap-6`}
-		>
-			{children}
-		</div>
-	);
+	return <div style={styles}>{children}</div>;
 };
