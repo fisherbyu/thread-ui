9a83f133c71f191fff83707578901186b177d731
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-01 00:24:11 -0600

    Render styles into images

--- src/internal-components/image/render-image.tsx
+++ src/internal-components/image/render-image.tsx
@@ -1,14 +1,22 @@
-import { isValidElement, ReactElement } from 'react';
+import React, { CSSProperties, isValidElement, ReactElement } from 'react';
 import { ImageProps } from '../../types';
 import { isHtmlImageProps } from './is-html-image-props';
 
-export const renderImage = (imageInput: ImageProps): ReactElement => {
+export const renderImage = (imageInput: ImageProps, styles?: CSSProperties): ReactElement => {
 	if (isValidElement(imageInput)) {
-		return imageInput;
+		const prevProps = imageInput.props as React.HTMLAttributes<HTMLElement>;
+		const newProps: React.HTMLAttributes<HTMLElement> = {
+			...prevProps,
+			style: {
+				...prevProps.style,
+				...styles,
+			},
+		};
+		return React.cloneElement(imageInput, newProps);
 	}
 	if (isHtmlImageProps(imageInput)) {
 		const { src, alt, width, height, ...restProps } = imageInput;
-		return <img src={src} alt={alt} width={width} height={height} {...restProps} />;
+		return <img src={src} alt={alt} width={width} height={height} style={{ ...restProps.styles, ...styles }} {...restProps} />;
 	}
 
 	console.warn('Invalid image props provided to renderImage');
