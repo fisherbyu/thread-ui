0a3ee5ab7d2ee5b6cd906b3ab9d8859e9376a949
Andrew Fisher <fisherandrew777@gmail.com>
2025-04-30 23:43:16 -0600

    Implement correct styles

--- src/components/image-panel/image-panel.tsx
+++ src/components/image-panel/image-panel.tsx
@@ -1,22 +1,90 @@
+import React from 'react';
 import { ImagePanelProps } from './image-panel.types';
 import { renderImage } from '../../internal-components';
 import { Subtitle, Text, Title } from '../typography';
+import { CSSProperties } from 'react';
+import { useResponsiveStyles } from '../../utils';
+import { ImageProps } from '../../types';
+
+// const renderPanelImage = (image: ImageProps, smImage?: ImageProps) => {
+// 	const styles: Record<string, CSSProperties> = {
+// 		smImage: {
+// 			position: 'relative',
+// 			display: useResponsiveStyles({ sm: 'none', lg: 'block' }),
+// 			borderRadius: '0.25rem',
+// 		},
+// 		standardImage: {
+// 			width: '100%',
+// 			height: '100%',
+// 			borderRadius: '0.25rem',
+// 		},
+// 		image: {
+// 			display: useResponsiveStyles({ sm: 'block', lg: 'none' }),
+// 			marginLeft: useResponsiveStyles({ sm: '0', md: 'auto' }),
+// 			marginRight: useResponsiveStyles({ sm: '0', md: 'auto' }),
+// 		},
+// 	};
+// 	const lgImage = renderImage(image);
+// 	const smallImage = renderImage(image);
+
+//     const minImage = React.cloneElement(lgImage, {
+//         style: {
+//             ...lgImage.props.style,
+
+//         }
+//     })
+
+// 	return (
+// 		<>
+// 			{renderImage(image)}
+// 			{smImage && renderImage(smImage)}
+// 		</>
+// 	);
+// };
 
 export const ImagePanel = ({ title, subtitle, contents, image, smImage, contentBelow, contentLeft }: ImagePanelProps) => {
+	const styles: Record<string, CSSProperties> = {
+		container: {
+			display: 'flex',
+			flexDirection: useResponsiveStyles({ sm: 'column-reverse', lg: 'row' }),
+			gap: useResponsiveStyles({ sm: '12px', lg: '20px' }),
+			justifyContent: 'center',
+			alignItems: 'stretch',
+			width: '100%',
+			marginRight: 'auto',
+			maxWidth: ' 1400px',
+			marginLeft: 'auto',
+			paddingRight: '2rem',
+			paddingLeft: '2rem',
+		},
+		imageBlock: {
+			width: useResponsiveStyles({ sm: '', lg: '66.666667%' }),
+		},
+		images: {
+			position: 'relative',
+			width: '100%',
+			height: '100%',
+		},
+		textBlock: {
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			marginLeft: 'auto',
+			marginRight: 'auto',
+			width: useResponsiveStyles({ sm: '100%', lg: '41.666667%' }),
+		},
+	};
 	return (
-		<div className="thread-container">
-			<div className="thread-flex thread-flex-col lg:thread-flex-row thread-justify-center thread-items-stretch">
-				<div className="thread-mt-6 md:thread-mt-8 lg:thread-mt-0 lg:thread-w-8/12 ">
-					<div className="thread-relative thread-w-full thread-h-full">{renderImage(image)}</div>
-				</div>
-				<div className="thread-flex thread-justify-center thread-items-center thread-order-first md:thread-mx-auto md:thread-w-11/12 md:thread-mt-12 lg:thread-w-5/12 lg:thread-px-6 lg:thread-order-none">
-					<div>
-						<Title inline>
-							{title}
-							{subtitle && <Subtitle>{subtitle}</Subtitle>}
-						</Title>
-						{contents?.map((txt) => <Text>{txt}</Text>)}
-					</div>
+		<div style={styles.container}>
+			<div style={styles.imageBlock}>{renderImage(image)} </div>
+			<div style={styles.textBlock}>
+				<div>
+					<Title inline>I'm Andrew!</Title>
+					<Text>
+						I'm a Master of Information Systems Managment Student at BYU interested in software development. I love serving
+						and getting to know people and using technology to create powerful solutions. I love music and music history
+						and am an avid cook and baker. Thanks for visiting my site!
+					</Text>
 				</div>
 			</div>
 		</div>
