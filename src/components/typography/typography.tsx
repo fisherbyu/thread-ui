c7c082796cbc94301bc671f487fd54f84b3aa17f
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-01 00:57:32 -0600

    Implement alignments

--- src/components/typography/typography.tsx
+++ src/components/typography/typography.tsx
@@ -2,10 +2,10 @@ import { CSSProperties, ReactNode } from 'react';
 
 export type TypographyProps = {
 	children: ReactNode;
+	align?: 'left' | 'center';
 };
 
 export type TitleProps = TypographyProps & {
-	align?: 'left' | 'center';
 	inline?: boolean;
 };
 export const Title = ({ children, align = 'left', inline = false }: TitleProps) => {
@@ -19,32 +19,35 @@ export const Title = ({ children, align = 'left', inline = false }: TitleProps) 
 	return <h1 style={styles}>{children}</h1>;
 };
 
-export const H1 = ({ children }: TypographyProps) => {
+export const H1 = ({ children, align = 'left' }: TypographyProps) => {
 	const styles: CSSProperties = {
 		fontSize: '2rem',
 		fontWeight: 600,
 		lineHeight: 1.3,
 		marginBottom: '32px',
+		textAlign: align,
 	};
 	return <h1 style={styles}>{children}</h1>;
 };
 
-export const H2 = ({ children }: TypographyProps) => {
+export const H2 = ({ children, align = 'left' }: TypographyProps) => {
 	const styles: CSSProperties = {
 		fontSize: '1.5rem',
 		fontWeight: 600,
 		lineHeight: 1.3,
 		marginBottom: '24px',
+		textAlign: align,
 	};
 	return <h2 style={styles}>{children}</h2>;
 };
 
-export const H3 = ({ children }: TypographyProps) => {
+export const H3 = ({ children, align = 'left' }: TypographyProps) => {
 	const styles: CSSProperties = {
 		fontSize: '1.25rem',
 		fontWeight: 600,
 		lineHeight: 1.3,
 		marginBottom: '16px',
+		textAlign: align,
 	};
 	return <h3 style={styles}>{children}</h3>;
 };
@@ -53,22 +56,24 @@ export type TextProps = TypographyProps & {
 	inline?: boolean;
 };
 
-export const Text = ({ children, inline = false }: TextProps) => {
+export const Text = ({ children, align = 'left', inline = false }: TextProps) => {
 	const Component = inline ? 'span' : 'p';
 	const styles: CSSProperties = {
 		fontSize: '1rem',
 		fontWeight: 350,
 		lineHeight: 1.5,
+		textAlign: align,
 	};
 	return <Component style={styles}>{children}</Component>;
 };
 
-export const Subtitle = ({ children }: TypographyProps) => {
+export const Subtitle = ({ children, align = 'left' }: TypographyProps) => {
 	const styles: CSSProperties = {
 		display: 'block',
 		fontSize: 'clamp(0.875rem, 0.75em, 1.5rem)',
 		color: '#878787',
 		marginTop: '0.2em',
+		textAlign: align,
 	};
 	return <span style={styles}>{children}</span>;
 };
