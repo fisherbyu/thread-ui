2257589c2b089ad491cbb865f5c6e134abd6399e
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-02 22:44:58 -0600

    Remove responsive

--- src/defaults/container/container.ts
+++ src/defaults/container/container.ts
@@ -1,11 +1,10 @@
 import { CSSProperties } from 'react';
-import { useResponsiveStyles } from '../../utils';
 
 export const CONTAINER_STYLES: CSSProperties = {
 	width: '100%',
 	marginRight: 'auto',
 	marginLeft: 'auto',
-	maxWidth: useResponsiveStyles({ sm: 'none', md: '1400px' }),
+	maxWidth: '1400px',
 	paddingRight: '2rem',
 	paddingLeft: '2rem',
 };
