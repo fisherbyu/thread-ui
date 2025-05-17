50b2063d43d5a2711fc89352b76e9e7782132b4e
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-17 16:20:20 -0600

    Simplify Button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -2,6 +2,7 @@
 import { CSSProperties, useState } from 'react';
 import { getUtilityColorValue, useTheme } from '../../utils';
 import { ButtonProps } from './button.types';
+import { makeStyles } from '../../functions';
 
 export const Button = ({
 	children,
@@ -13,11 +14,9 @@ export const Button = ({
 	disabled = false,
 }: ButtonProps) => {
 	const { theme } = useTheme();
-	const [isHovered, setIsHovered] = useState(false);
-
 	const colorValue = getUtilityColorValue(color);
 
-	const buttonStyles: CSSProperties = {
+	const buttonClasses = makeStyles({
 		padding: `${theme.space * 2}px`,
 		width: fullWidth ? '100%' : 'fit-content',
 		transition: 'background-color 0.2s ease',
@@ -32,22 +31,14 @@ export const Button = ({
 		cursor: 'pointer',
 		userSelect: 'none',
 		opacity: disabled ? 0.6 : 1,
-	};
-
-	const hoverButton: CSSProperties = {
-		...buttonStyles,
-		backgroundColor: disabled ? colorValue : theme.colors.white,
-		color: disabled ? theme.colors.white : colorValue,
-	};
+		hover: {
+			backgroundColor: disabled ? colorValue : theme.colors.white,
+			color: disabled ? theme.colors.white : colorValue,
+		},
+	});
 
 	return (
-		<button
-			type={type}
-			style={isHovered ? hoverButton : buttonStyles}
-			onMouseEnter={() => setIsHovered(true)}
-			onMouseLeave={() => setIsHovered(false)}
-			onClick={disabled ? undefined : onClick}
-		>
+		<button type={type} className={buttonClasses} onClick={disabled ? undefined : onClick}>
 			{children}
 		</button>
 	);
