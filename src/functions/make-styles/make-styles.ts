7fc9c488b28131bbbe863cb59a64709bb10f01ad
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-17 16:20:12 -0600

    Add hover styles to make styles

--- src/functions/make-styles/make-styles.ts
+++ src/functions/make-styles/make-styles.ts
@@ -1,14 +1,26 @@
 import { css } from '@emotion/css';
 import { CSSProperties } from 'react';
 
-export const makeStyles = (styles: CSSProperties) => {
+const translateCSSProperties = (styles: CSSProperties): string => {
+	return Object.entries(styles)
+		.map(([key, value]) => {
+			// Convert camelCase to kebab-case
+			const cssKey = key.replace(/([A-Z])/g, '-$1').toLowerCase();
+			return `${cssKey}: ${value};`;
+		})
+		.join('\n');
+};
+
+interface MakeStylesProps extends CSSProperties {
+	hover?: CSSProperties;
+}
+
+export const makeStyles = (styles: MakeStylesProps) => {
+	const { hover, ...baseProps } = styles;
+	const baseStyles = translateCSSProperties(baseProps);
+
 	return css`
-		${Object.entries(styles)
-			.map(([key, value]) => {
-				// Convert camelCase to kebab-case
-				const cssKey = key.replace(/([A-Z])/g, '-$1').toLowerCase();
-				return `${cssKey}: ${value};`;
-			})
-			.join('\n')}
+		${baseStyles}
+		${hover ? `&:hover { ${translateCSSProperties(hover)} }` : ''}
 	`;
 };
