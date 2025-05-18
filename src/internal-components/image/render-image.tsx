eaf98252704f6b41c661eaad77d1325acf2c7b90
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-17 18:12:14 -0600

    Allow classes to be passed as well as styles

--- src/internal-components/image/render-image.tsx
+++ src/internal-components/image/render-image.tsx
@@ -2,11 +2,12 @@ import React, { CSSProperties, isValidElement, ReactElement } from 'react';
 import { ImageProps } from '../../types';
 import { isHtmlImageProps } from './is-html-image-props';
 
-export const renderImage = (imageInput: ImageProps, styles?: CSSProperties): ReactElement => {
+export const renderImage = (imageInput: ImageProps, styles?: CSSProperties, className?: string): ReactElement => {
 	if (isValidElement(imageInput)) {
 		const prevProps = imageInput.props as React.HTMLAttributes<HTMLElement>;
 		const newProps: React.HTMLAttributes<HTMLElement> = {
 			...prevProps,
+			className: className ? (prevProps.className ? `${prevProps.className} ${className}` : className) : prevProps.className,
 			style: {
 				...prevProps.style,
 				...styles,
@@ -14,9 +15,20 @@ export const renderImage = (imageInput: ImageProps, styles?: CSSProperties): Rea
 		};
 		return React.cloneElement(imageInput, newProps);
 	}
+
 	if (isHtmlImageProps(imageInput)) {
-		const { src, alt, width, height, ...restProps } = imageInput;
-		return <img src={src} alt={alt} width={width} height={height} style={{ ...restProps.styles, ...styles }} {...restProps} />;
+		const { src, alt, width, height, className: imgClassName, ...restProps } = imageInput;
+		return (
+			<img
+				src={src}
+				alt={alt}
+				width={width}
+				height={height}
+				className={className ? (imgClassName ? `${imgClassName} ${className}` : className) : imgClassName}
+				style={{ ...restProps.styles, ...styles }}
+				{...restProps}
+			/>
+		);
 	}
 
 	console.warn('Invalid image props provided to renderImage');
