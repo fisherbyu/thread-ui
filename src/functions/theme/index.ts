7bf09d4a621da365763d64d08370a4c4e28577bf
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-21 22:41:04 -0600

    Update Exports

--- src/functions/theme/index.ts
+++ src/functions/theme/index.ts
@@ -1,3 +1,5 @@
 export { createAppliedTheme, createTheme } from './create-theme';
 export { getCurrentTheme } from './current-theme';
 export { ThemeProvider, useTheme, useThemeMode, useThemeOption, useThemeToggle } from './use-theme';
+export { setTheme } from './get-theme';
+export type { ThemeConfig } from './get-theme';
