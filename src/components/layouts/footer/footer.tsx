798833e03b2623e1f3a224ed1730b8684eff98b5
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-24 00:50:02 -0600

    Update Styling

--- src/components/layouts/footer/footer.tsx
+++ src/components/layouts/footer/footer.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { FooterProps } from './footer.types';
+import { CONTAINER_STYLES } from '@/defaults';
+import { Divider, Subtitle } from '@/components';
+import { useThreadStyleObjects, ThreadTheme } from '@/functions';
 import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo, TwitterLogo } from '@phosphor-icons/react';
-import { CSSProperties } from 'react';
-import { CONTAINER_STYLES } from '../../../defaults';
-import { Divider } from '../../ui';
-import { useThreadStyleObjects } from '../../../functions';
 
 export const Footer = ({ logo, caption, facebookLink, instagramLink, twitterLink, githubLink, linkedInLink }: FooterProps) => {
 	const styles = useThreadStyleObjects({
@@ -36,36 +35,39 @@ export const Footer = ({ logo, caption, facebookLink, instagramLink, twitterLink
 			width: '48px',
 			height: '48px',
 		},
+		links: {
+			color: ThreadTheme.text.primary,
+		},
 	});
 	return (
 		<footer className={styles.footer}>
 			<Divider />
 			<div className={styles.footerContents}>
 				{logo && logo}
-				{caption}
+				{caption && <Subtitle>{caption}</Subtitle>}
 				<div className={styles.logoBlock}>
 					{facebookLink && (
-						<a href={facebookLink}>
+						<a className={styles.links} href={facebookLink}>
 							<FacebookLogo size={32} weight="fill" />
 						</a>
 					)}
 					{instagramLink && (
-						<a href={instagramLink}>
+						<a className={styles.links} href={instagramLink}>
 							<InstagramLogo size={32} weight="fill" />
 						</a>
 					)}
 					{twitterLink && (
-						<a href={twitterLink}>
+						<a className={styles.links} href={twitterLink}>
 							<TwitterLogo size={32} weight="fill" />
 						</a>
 					)}
 					{githubLink && (
-						<a href={githubLink}>
+						<a className={styles.links} href={githubLink}>
 							<GithubLogo size={32} weight="fill" />
 						</a>
 					)}
 					{linkedInLink && (
-						<a href={linkedInLink}>
+						<a className={styles.links} href={linkedInLink}>
 							<LinkedinLogo size={32} weight="fill" />
 						</a>
 					)}
