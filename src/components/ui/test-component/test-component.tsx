f99b850b30959b52746c2bc3c6bdc93a44f14dae
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-23 19:49:15 -0600

    Add new theme provider

--- src/components/ui/test-component/test-component.tsx
+++ src/components/ui/test-component/test-component.tsx
@@ -1,3 +1,20 @@
+import { CONTAINER_STYLES } from '../../../defaults';
+import { makeStyleObject } from '../../../functions';
+import { getThemeValue } from '../../../functions/theme/get-theme';
+
 export const TestComponent = () => {
-	return <div className="thread-container thread-bg-primary thread-h-5"></div>;
+	const styles = makeStyleObject({
+		container: CONTAINER_STYLES,
+		block: {
+			backgroundColor: getThemeValue().background,
+		},
+		text: {
+			color: getThemeValue().primary.main,
+		},
+	});
+	return (
+		<div className={`${styles.container} ${styles.block}`}>
+			<p className={styles.text}>TEST 123</p>
+		</div>
+	);
 };
