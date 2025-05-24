3d554b2cdf7b625b6334f3c2acb680b0f60ce3c6
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-24 01:26:04 -0600

    Create Colored Text Options Type

--- src/types/colors/utility-color-options.types.ts
+++ src/types/colors/utility-color-options.types.ts
@@ -1,1 +1,4 @@
+import { TextColorOptions } from '../theme';
+
 export type UtilityColorOptions = 'primary' | 'secondary' | 'tertiary' | 'black' | 'gray' | 'success' | 'error' | 'info' | 'text';
+export type ColoredTextOptions = (Omit<UtilityColorOptions, 'text'> & Omit<TextColorOptions, 'secondary'>) | 'text-secondary';
