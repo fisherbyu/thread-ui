4415775efa8ca36e7049227c2aea0b6f8f1bc418
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-23 19:05:24 -0600

    Move old stuff into own folder

--- src/functions/theme/index.ts
+++ src/functions/theme/index.ts
@@ -1,5 +1,3 @@
-export { createAppliedTheme, createTheme } from './create-theme';
-export { getCurrentTheme } from './current-theme';
-export { ThemeProvider, useTheme, useThemeMode, useThemeOption, useThemeToggle } from './use-theme';
 export { setTheme } from './get-theme';
 export type { ThemeConfig } from './get-theme';
+export * from './old';
