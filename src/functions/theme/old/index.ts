4d31fec8c1209ed2ef308b0f3131e92e77e52e38
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-23 21:19:24 -0600

    Remove old implementation

--- src/functions/theme/old/index.ts
+++
@@ -1,3 +0,0 @@
-export { createAppliedTheme, createTheme } from './create-theme';
-export { getCurrentTheme } from './current-theme';
-export { ThemeProvider, useTheme, useThemeMode, useThemeOption, useThemeToggle } from './use-theme';
