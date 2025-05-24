4687cf5e56384bd58dbac8665a4d2102cc3dad1e
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-24 01:32:01 -0600

    Add color to typography

--- src/components/typography/typography.tsx
+++ src/components/typography/typography.tsx
@@ -1,54 +1,61 @@
 'use client';
+import { ColoredTextOptions, UtilityColorOptions } from '@/types';
+import { getColoredTextColor } from '@/utils';
 import { CSSProperties, ReactNode } from 'react';
 
 export type TypographyProps = {
 	children: ReactNode;
 	align?: 'left' | 'center';
+	color?: ColoredTextOptions;
 };
 
 export type TitleProps = TypographyProps & {
 	inline?: boolean;
 };
-export const Title = ({ children, align = 'left', inline = false }: TitleProps) => {
+export const Title = ({ children, align = 'left', inline = false, color = 'standard' }: TitleProps) => {
 	const styles: CSSProperties = {
 		fontSize: '3rem',
 		fontWeight: 700,
 		lineHeight: 1.3,
 		marginBottom: inline ? 0 : '40px',
 		textAlign: align,
+		color: getColoredTextColor(color),
 	};
 	return <h1 style={styles}>{children}</h1>;
 };
 
-export const H1 = ({ children, align = 'left' }: TypographyProps) => {
+export const H1 = ({ children, align = 'left', color = 'standard' }: TypographyProps) => {
 	const styles: CSSProperties = {
 		fontSize: '2rem',
 		fontWeight: 600,
 		lineHeight: 1.3,
 		marginBottom: '32px',
 		textAlign: align,
+		color: getColoredTextColor(color),
 	};
 	return <h1 style={styles}>{children}</h1>;
 };
 
-export const H2 = ({ children, align = 'left' }: TypographyProps) => {
+export const H2 = ({ children, align = 'left', color = 'standard' }: TypographyProps) => {
 	const styles: CSSProperties = {
 		fontSize: '1.5rem',
 		fontWeight: 600,
 		lineHeight: 1.3,
 		marginBottom: '24px',
 		textAlign: align,
+		color: getColoredTextColor(color),
 	};
 	return <h2 style={styles}>{children}</h2>;
 };
 
-export const H3 = ({ children, align = 'left' }: TypographyProps) => {
+export const H3 = ({ children, align = 'left', color = 'standard' }: TypographyProps) => {
 	const styles: CSSProperties = {
 		fontSize: '1.25rem',
 		fontWeight: 600,
 		lineHeight: 1.3,
 		marginBottom: '16px',
 		textAlign: align,
+		color: getColoredTextColor(color),
 	};
 	return <h3 style={styles}>{children}</h3>;
 };
@@ -57,24 +64,25 @@ export type TextProps = TypographyProps & {
 	inline?: boolean;
 };
 
-export const Text = ({ children, align = 'left', inline = false }: TextProps) => {
+export const Text = ({ children, align = 'left', inline = false, color = 'standard' }: TextProps) => {
 	const Component = inline ? 'span' : 'p';
 	const styles: CSSProperties = {
 		fontSize: '1rem',
 		fontWeight: 350,
 		lineHeight: 1.5,
 		textAlign: align,
+		color: getColoredTextColor(color),
 	};
 	return <Component style={styles}>{children}</Component>;
 };
 
-export const Subtitle = ({ children, align = 'left' }: TypographyProps) => {
+export const Subtitle = ({ children, align = 'left', color = 'secondary' }: TypographyProps) => {
 	const styles: CSSProperties = {
 		display: 'block',
 		fontSize: 'clamp(0.875rem, 0.75em, 1.5rem)',
-		color: '#878787',
 		marginTop: '0.2em',
 		textAlign: align,
+		color: getColoredTextColor(color),
 	};
 	return <span style={styles}>{children}</span>;
 };
