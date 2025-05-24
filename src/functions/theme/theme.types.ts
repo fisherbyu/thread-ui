88018133c768cd4a7de5dd7d6a867c4407e4ac22
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-23 23:47:00 -0600

    Assemble theme types

---
+++ src/functions/theme/theme.types.ts
@@ -0,0 +1,67 @@
+import { DeepPartial } from '@/utils';
+
+type ColorShades = {
+	light: string;
+	main: string;
+	dark: string;
+};
+
+export type TextColors = {
+	primary: string;
+	secondary: string;
+	disabled: string;
+};
+
+export type UtilitySizes = {
+	sm: string;
+	md: string;
+	lg: string;
+};
+
+export type Theme = {
+	// Color Palette
+	primary: ColorShades;
+	secondary: ColorShades;
+	tertiary: ColorShades;
+
+	// Neutral Colors
+	white: string;
+	black: string;
+	gray: ColorShades;
+
+	// Status Colors
+	success: ColorShades;
+	warning: ColorShades;
+	error: ColorShades;
+	info: ColorShades;
+
+	// Surface Colors
+	background: string;
+	surface: string;
+	elevated: string;
+	structure: string;
+
+	// Text Colors
+	text: TextColors;
+
+	// Sizing/Structure
+	borderRadius: UtilitySizes;
+	borderSize: UtilitySizes;
+};
+
+export type DarkModeColors = {
+	// Surface Colors
+	background: string;
+	surface: string;
+	elevated: string;
+	structure: string;
+
+	// Text Colors
+	text: TextColors;
+};
+
+export type ThemeConfigBase = Theme & {
+	darkMode: DarkModeColors;
+};
+
+export type ThemeConfig = DeepPartial<ThemeConfigBase>;
