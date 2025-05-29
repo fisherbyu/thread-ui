0c9e7d83d35c4b08ad2273af01bd24c42bd33b13
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-28 21:57:19 -0600

    Fix union type

--- src/types/colors/utility-color-options.types.ts
+++ src/types/colors/utility-color-options.types.ts
@@ -1,4 +1,4 @@
 import { TextColorOptions } from '../theme';
 
 export type UtilityColorOptions = 'primary' | 'secondary' | 'tertiary' | 'black' | 'gray' | 'success' | 'error' | 'info' | 'text';
-export type ColoredTextOptions = (Omit<UtilityColorOptions, 'text'> & Omit<TextColorOptions, 'secondary'>) | 'text-secondary';
+export type ColoredTextOptions = Exclude<UtilityColorOptions, 'text'> | 'standard' | 'disabled' | 'accent' | 'text-secondary';
