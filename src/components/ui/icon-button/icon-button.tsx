e8a72fcc99819ece0ad6b5dd3a324c4f74a208f7
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-28 23:15:55 -0600

    Implement new functionality

--- src/components/ui/icon-button/icon-button.tsx
+++ src/components/ui/icon-button/icon-button.tsx
@@ -1,22 +1,28 @@
 'use client';
-import { Icon } from '../icon/icon';
+import { Icon, Button } from '@/components';
 import { IconButtonProps } from './icon-button.types';
-import { Button } from '../button';
+import { getUtilityFontSize, getUtilityIconSize, getUtilitySizeValue } from '@/utils';
+import { useThreadStyles } from '@/functions';
 
 export const IconButton = ({
 	children,
 	fullWidth,
 	color = 'primary',
+	size = 'md',
 	onClick,
 	type = 'button',
 	margin,
 	disabled = false,
 	name,
 }: IconButtonProps) => {
+	const styles = useThreadStyles({
+		marginLeft: `${getUtilitySizeValue(size) - 2}px`,
+		fontSize: getUtilityFontSize(size),
+	});
 	return (
 		<Button fullWidth={fullWidth} color={color} onClick={onClick} type={type} margin={margin} disabled={disabled}>
-			<Icon name={name} size={24} />
-			{children}
+			<Icon name={name} size={getUtilityIconSize(size)} />
+			<div className={styles}>{children}</div>
 		</Button>
 	);
 };
