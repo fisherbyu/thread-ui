a22911ff0c80b8cbf59b42c72982b7a8925d630a
Andrew Fisher <fisherandrew777@gmail.com>
2025-05-28 23:51:04 -0600

    Refactor to use style generator hook

--- src/components/media/info-card/info-card.tsx
+++ src/components/media/info-card/info-card.tsx
@@ -1,31 +1,77 @@
 'use client';
+import { ThreadTheme, useThreadStyleObjects } from '@/functions';
 import { InfoCardProps } from './info-card.types';
 
 export const InfoCard = ({ title, url, icon, img }: InfoCardProps) => {
+	const styles = useThreadStyleObjects({
+		container: {
+			margin: 'auto',
+			borderRadius: ThreadTheme.borderRadius.lg,
+			border: `${ThreadTheme.borderSize.md} ${ThreadTheme.structure} solid`,
+			marginTop: '0.75rem',
+			marginBottom: '0.75rem',
+			maxHeight: '15rem',
+			overflow: 'hidden',
+			width: '391px',
+			height: '241px',
+			backgroundColor: ThreadTheme.background,
+			hover: {
+				backgroundColor: ThreadTheme.surface,
+			},
+		},
+		link: {
+			cursor: 'pointer',
+		},
+		imageWrapper: {
+			overflow: 'hidden',
+			width: '100%',
+			height: '83.333333%',
+		},
+		image: {
+			height: 'auto',
+			minHeight: '100%',
+			width: 'auto',
+			minWidth: '100%',
+		},
+		captionBlock: {
+			padding: '0.5rem',
+			height: '16.666667%',
+		},
+		caption: {
+			display: 'flex',
+			justifyContent: 'flex-start',
+			alignItems: 'center',
+		},
+		captionIcon: {
+			paddingRight: '0.25rem',
+		},
+		captionText: {
+			overflow: 'hidden',
+			textOverflow: 'ellipsis',
+			whiteSpace: 'nowrap',
+			fontSize: '0.875rem',
+			lineHeight: '1.25rem',
+			color: ThreadTheme.text.standard,
+		},
+	});
 	return (
-		<div className="thread-mx-auto thread-rounded-lg thread-border thread-bg-card thread-text-card-foreground thread-shadow-sm thread-my-3 thread-overflow-hidden thread-max-h-60 thread-w-[391px] thread-h-[241px]">
-			<a href={url}>
-				<span className=" thread-cursor-pointer">
-					<div className="thread-overflow-hidden thread-w-full thread-h-5/6">
-						<img
-							className="thread-h-auto thread-min-h-full thread-w-auto thread-min-w-full"
-							src={img}
-							alt="recipe cover"
-						/>
-					</div>
-					<div className="thread-p-2 thread-h-1/6">
-						<span className="thread-flex thread-justify-start thread-items-center">
-							{icon.type === 'emoji' ? (
-								<span className=" thread-pr-1" role="img">
-									{JSON.parse(`"${icon.content}"`)}
-								</span>
-							) : (
-								<img className="thread-pr-1" height={23} width={23} src={icon.content} alt="Recipe Logo" />
-							)}
-							<p className="thread-truncate thread-text-sm">{title}</p>
-						</span>
-					</div>
-				</span>
+		<div className={styles.container}>
+			<a href={url} className={styles.link}>
+				<div className={styles.imageWrapper}>
+					<img className={styles.image} src={img} alt="Article Cover Image" />
+				</div>
+				<div className={styles.captionBlock}>
+					<span className={styles.caption}>
+						{icon.type === 'emoji' ? (
+							<span className={styles.captionIcon} role="img">
+								{JSON.parse(`"${icon.content}"`)}
+							</span>
+						) : (
+							<img className={styles.captionIcon} height={23} width={23} src={icon.content} alt="Article Icon" />
+						)}
+						<p className="thread-truncate thread-text-sm">{title}</p>
+					</span>
+				</div>
 			</a>
 		</div>
 	);
