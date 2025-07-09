2b585e839107869438cc8785954abc855f98cf58
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-09 16:12:09 -0600

    Configure Panda CSS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Import Tailwind + Panda Styles
 import './styles/thread.css';
 import './styles/styles.css';
-import './styles/panda.css';
+import './styles/index.css';
 
 // Export Components
 export * from './components';
