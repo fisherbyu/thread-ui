1defaae727d04de58d3d5cce0b49a9b6001aa9a4
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-10 00:50:29 -0600

    Update imports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // Import Tailwind + Panda Styles
 import './styles/thread.css';
 import './styles/styles.css';
-import './styles/index.css';
+import './styled-system/styles.css';
 
 // Export Components
 export * from './components';
