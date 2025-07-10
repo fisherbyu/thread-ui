477f91b27a7e16c6af4483de2dcf388751a39173
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-10 16:58:31 -0600

    Configure Panda Process

--- panda.config.ts
+++ panda.config.ts
@@ -4,18 +4,9 @@ export default defineConfig({
 	// Whether to use css reset
 	preflight: true,
 
-	// Where to look for your css declarations
+	// Build Declarations
 	include: ['./src/**/*.{ts,tsx,js,jsx}'],
-
-	// Files to exclude
-	exclude: [],
 
 	// The output directory for your css system
 	outdir: 'src/styled-system',
-
-	layers: {
-		base: 'panda_base',
-		recipes: 'panda_recipes',
-		utilities: 'panda_utilities',
-	},
 });
