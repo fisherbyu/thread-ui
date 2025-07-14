901c3b65c423eeb590565ef46de8d0813b1f503e
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-13 23:44:47 -0600

    Update CSS Generation

--- src/components/navigation/nav-menu/items/base-item/base-item.tsx
+++ src/components/navigation/nav-menu/items/base-item/base-item.tsx
@@ -1,43 +1,55 @@
-'use client';
 import { BaseItemProps } from './base-item.types';
 import { LinkWrapper } from '@/internal-components';
-import { NavMenuStyles } from '../../nav-menu-styles';
-import { ThreadTheme, generateStyleObject } from '@/functions';
+import { css, cva } from '@/styled-system/css';
 
-export const BaseItem = ({ children, href, padding, onMouseEnter, onMouseLeave, isDropdownItem }: BaseItemProps) => {
-	const styles = generateStyleObject({
-		li: {
+export const BaseItem = ({ children, href, halfPadding, onMouseEnter, onMouseLeave, isDropdownItem }: BaseItemProps) => {
+	const styles = {
+		li: css({
 			display: 'flex',
 			alignItems: 'center',
 			listStyleType: 'none',
-		},
-		link: {
-			position: { sm: 'static', lg: 'relative' },
-			display: 'inline-flex',
-			flexDirection: 'column',
-			height: { sm: 'auto', lg: '2.5rem' },
-			alignItems: 'center',
-			justifyContent: 'center',
-			borderRadius: '0.375rem',
-			color: ThreadTheme.text.standard,
-			fontSize: '0.875rem',
-			fontWeight: 500,
-			padding: padding ?? `${NavMenuStyles.paddingY / 2}px ${NavMenuStyles.paddingX}px`,
-			width: isDropdownItem ? { sm: 'fit-content', lg: '100%' } : { sm: '100%', lg: 'fit-content' },
-			textDecoration: 'none',
-			transition: 'all 150ms ease-in-out',
-			margin: 'auto',
-			hover: {
-				backgroundColor: ThreadTheme.elevated,
+		}),
+		link: cva({
+			base: {
+				position: { sm: 'static', lg: 'relative' },
+				display: 'inline-flex',
+				flexDirection: 'column',
+				height: { sm: 'auto', lg: '2.5rem' },
+				alignItems: 'center',
+				justifyContent: 'center',
+				borderRadius: '0.375rem',
+				color: 'text.standard',
+				fontSize: '0.875rem',
+				fontWeight: 500,
+				textDecoration: 'none',
+				transition: 'all 150ms ease-in-out',
+				margin: 'auto',
+				_hover: {
+					backgroundColor: 'elevated',
+				},
 			},
-		},
-	});
+			variants: {
+				halfPadding: {
+					true: { padding: '8px' },
+					false: { padding: '4px' },
+				},
+				isDropdownItem: {
+					true: { width: { base: 'fit-content', lg: '100%' } },
+					false: { width: { base: '100%', lg: 'fit-content' } },
+				},
+			},
+			defaultVariants: {
+				halfPadding: false,
+				isDropdownItem: false,
+			},
+		}),
+	};
 
 	// Handle hover state
 
 	return (
 		<li className={styles.li}>
-			<LinkWrapper link={href} className={styles.link}>
+			<LinkWrapper link={href} className={styles.link({ halfPadding, isDropdownItem })}>
 				{children}
 			</LinkWrapper>
 		</li>
