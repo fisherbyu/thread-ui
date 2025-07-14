49347d715090f8754414d9d08f6543fb69d2400a
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-13 23:44:51 -0600

    Update props

--- src/components/navigation/nav-menu/items/base-item/base-item.types.ts
+++ src/components/navigation/nav-menu/items/base-item/base-item.types.ts
@@ -3,8 +3,8 @@ import { ReactNode } from 'react';
 export type BaseItemProps = {
 	children: ReactNode;
 	href: string;
-	padding?: string;
 	onMouseEnter?: (event: React.MouseEvent<HTMLLIElement>) => void;
 	onMouseLeave?: (event: React.MouseEvent<HTMLLIElement>) => void;
+	halfPadding: boolean;
 	isDropdownItem?: boolean;
 };
