c193bc24f63042667e894af2bce6fdfa278b270f
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-15 00:24:49 -0600

    Upgrade CSS Generation

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -1,12 +1,9 @@
-import { getUtilityColorValue, getUtilityFontSize, getUtilitySizeValue } from '@/utils';
+import { button } from '@/styled-system/recipes';
 import { ButtonProps } from './button.types';
-import { ThreadTheme, generateStyles } from '@/functions';
-import { css } from '@/styled-system/css';
-import { CSSProperties } from 'react';
 
 export const Button = ({
 	children,
-	fullWidth,
+	fullWidth = false,
 	color = 'primary',
 	size = 'md',
 	onClick,
@@ -14,42 +11,15 @@ export const Button = ({
 	margin,
 	disabled = false,
 }: ButtonProps) => {
-	const colorValue = getUtilityColorValue(color);
-
-	const styles = {
-		button: css({
-			borderRadius: 'md',
-			borderStyle: 'solid',
-			borderWidth: 'md',
-			transition: 'background-color 0.2s ease',
-			color: 'white',
-			display: 'flex',
-			justifyContent: 'center',
-			alignItems: 'center',
-			cursor: 'pointer',
-			userSelect: 'none',
-		}),
-	};
-
-	const staticStyles: CSSProperties = {
-		backgroundColor: colorValue,
-		borderColor: color == 'black' ? ThreadTheme.white : colorValue,
-		fontSize: getUtilityFontSize(size),
-		opacity: disabled ? 0.6 : 1,
-		padding: `${getUtilitySizeValue(size)}px`,
-		width: fullWidth ? '100%' : 'fit-content',
-	};
-
-	const buttonClasses = generateStyles({
-		hover: {
-			backgroundColor: disabled ? colorValue : color == 'black' ? ThreadTheme.white : ThreadTheme.background,
-			color: disabled ? ThreadTheme.background : colorValue,
-			borderColor: color == 'black' ? ThreadTheme.black : colorValue,
-		},
+	const buttonClasses = button({
+		color,
+		size,
+		fullWidth,
+		disabled,
 	});
 
 	return (
-		<button type={type} className={buttonClasses} onClick={disabled ? undefined : onClick}>
+		<button type={type} className={buttonClasses} onClick={disabled ? undefined : onClick} style={margin ? { margin } : undefined}>
 			{children}
 		</button>
 	);
