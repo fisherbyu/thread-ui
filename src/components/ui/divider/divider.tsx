bcac903d7ae6860613fc3b7c619214d5c65bb826
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-14 23:15:57 -0600

    Create Variants

--- src/components/ui/divider/divider.tsx
+++ src/components/ui/divider/divider.tsx
@@ -1,23 +1,22 @@
 'use client';
 import { ThreadTheme, generateStyles } from '@/functions';
 import { DividerProps } from './divider.types';
+import { cva } from '@/styled-system/css';
+
+const dividerWeight = cva({
+	variants: {
+		weight: {
+			light: { height: '0.5px' },
+			standard: { height: '1px' },
+			heavy: { height: '2px' },
+		},
+	},
+	defaultVariants: {
+		weight: 'standard',
+	},
+});
 
 export const Divider = ({ width, marginY, weight }: DividerProps) => {
-	let dividerWeight: number;
-	switch (weight) {
-		case 'light':
-			dividerWeight = 0.5;
-			break;
-		case 'standard':
-			dividerWeight = 1;
-			break;
-		case 'bold':
-			dividerWeight = 2;
-			break;
-		default:
-			dividerWeight = 1;
-	}
-
 	const styles = generateStyles({
 		width: width || '75%',
 		marginTop: marginY || '16px',
