a8e7077553536c2ba9d271e73d191fec7cda466e
Andrew Fisher <fisherandrew777@gmail.com>
2025-07-27 00:03:30 -0600

    Create TextInput

---
+++ src/components/form-elements/text-input/text-input.tsx
@@ -0,0 +1,40 @@
+import { FormLabel } from '../form-label';
+import { TextInputProps } from './text-input.types';
+import { InputWrapper } from '../input-wrapper';
+import { baseInputStyles } from '../styles';
+import { cx, css } from '@/styled-system/css';
+
+export const TextInput = ({ name, id = name, title, value, required, placeholder, multiline = false, onChange }: TextInputProps) => {
+	const style = css({
+		minHeight: '100px',
+		resize: 'vertical',
+	});
+	return (
+		<InputWrapper>
+			{title && <FormLabel id={id} name={name} title={title} />}
+			{multiline ? (
+				<textarea
+					id={id}
+					name={name}
+					required={required}
+					value={value}
+					onChange={onChange}
+					placeholder={placeholder}
+					rows={3}
+					className={cx(baseInputStyles(), style)}
+				/>
+			) : (
+				<input
+					type="text"
+					id={id}
+					name={name}
+					required={required}
+					value={value}
+					onChange={onChange}
+					placeholder={placeholder}
+					className={baseInputStyles()}
+				/>
+			)}
+		</InputWrapper>
+	);
+};
