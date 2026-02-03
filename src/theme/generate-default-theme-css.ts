c88103afb939a6b1ad4e654037c7cf524ac5f7ca
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-03 16:43:05 -0700

    Create generator function

--- src/theme/generate-default-theme-css.ts
+++ src/theme/generate-default-theme-css.ts
@@ -1,6 +1,17 @@
-import { ThemeConfigFull } from '@/types/theme/theme.types';
+import { ModeColors, Theme, ThemeConfigFull } from '@/types/theme/theme.types';
 import { DefaultThreadThemeConfig } from './default-thread-theme-config';
+import { ThreadThemeCssNames, DarkModeVariablesCssNames } from './thread-theme';
 
-export const generateDefaultThemeCss = <T>(defaultTheme: T) => {};
-
-const css = generateDefaultThemeCss<ThemeConfigFull>(DefaultThreadThemeConfig);
+export const generateDefaultThemeCss = (
+	defaultThemeConfig: ThemeConfigFull,
+	themeVariableNames: Theme,
+	lightModeVariableNames: ModeColors,
+	darkModeVariableNames: ModeColors
+) => {
+	// Generate Basic Theme Variables
+	// Generate Light Mode Variables
+	// Generate Dark Mode Variables
+	// Apply Light Mode Colors
+	// Apply Dark Mode Colors when Dark Mode Active
+	// Apply Dark Mode Colors when Dark Mode Applied
+};
