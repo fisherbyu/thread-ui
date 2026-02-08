cb4ffd93ff3e6bebb6cccbf9ec220e9509e7cca2
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-07 22:06:00 -0700

    Export all exported types

--- src/types/theme/index.ts
+++ src/types/theme/index.ts
@@ -1,10 +1,1 @@
-export type {
-	Theme,
-	TextColors,
-	TextColorOptions,
-	UtilitySizes,
-	ThemeConfig,
-	ThemeConfigFull,
-	ModeColors,
-	UtilitySizeOptions,
-} from './theme.types';
+export * from './theme.types';
