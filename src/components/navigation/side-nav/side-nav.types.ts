9c0c397e04c747f291012fb864ad8e47f9b99889
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-13 18:11:13 -0700

    Update whitespace

--- src/components/navigation/side-nav/side-nav.types.ts
+++ src/components/navigation/side-nav/side-nav.types.ts
@@ -1,8 +1,9 @@
 import { ReactNode } from 'react';
 import { SideNavItemProps } from './side-nav-item';
+
 export type SideNavProps = {
-    logo?: ReactNode;
-    links: SideNavItemProps[];
-    controls?: ReactNode;
-    basePath?: string;
+	logo?: ReactNode;
+	links: SideNavItemProps[];
+	controls?: ReactNode;
+	basePath?: string;
 };
