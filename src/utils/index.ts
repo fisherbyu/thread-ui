601d530733872cd79e9815dd77942a30c794aabb
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-14 00:30:57 -0700

    Move theme utils into container directory

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,4 @@
-export * from './deep-merge';
-export * from './get-colored-text-color';
-export * from './get-text-color';
-export * from './get-utility-color';
-export * from './get-utility-font-size';
-export * from './get-utility-icon-size';
-export * from './get-utility-size-value';
+export * from './data-manipulation';
+export * from './theme-utilities';
 
 export * from './use-pathname';
