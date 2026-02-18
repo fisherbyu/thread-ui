34d3664ecfdddde3d37aba431d9ca4aba2c4ee8d
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-17 18:26:47 -0700

    Create and export image type checker

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 export * from './context';
 export * from './data-manipulation';
 export * from './hooks';
+export * from './misc';
 export * from './theme-utilities';
---
+++ src/utils/misc/is-file-image-type/index.ts
@@ -0,0 +1,1 @@
+export * from './is-file-image-type';
