8772384a06c3aea22b60640396ddadf1f5912241
Andrew Fisher <fisherandrew777@gmail.com>
2026-02-26 00:01:01 -0700

    Create skeleton loader props

---
+++ src/components/ui/loaders/skeleton/skeleton.types.ts
@@ -0,0 +1,8 @@
+export type SkeletonProps = {
+	/** Width of the skeleton block @default '100%' */
+	w?: string;
+	/** Height of the skeleton block @default '1rem' */
+	h?: string;
+	/** Whether to render as a circle (e.g. avatars) @default false */
+	round?: boolean;
+};
