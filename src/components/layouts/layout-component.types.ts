220144b07b68e90064733b9acaff00b85ca90ff8
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-02 17:20:39 -0700

    Create responsive value type to build using breakpoints

--- src/components/layouts/layout-component.types.ts
+++ src/components/layouts/layout-component.types.ts
@@ -1,3 +1,7 @@
+import type { BreakpointToken } from '@/styled-system/tokens';
+
+export type ResponsiveValue<T> = T | Partial<Record<BreakpointToken, T>>;
+
 export type LayoutComponentProps = {
 	container?: boolean;
 };
