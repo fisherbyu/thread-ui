8f51963350d4d313ec990b22d94f20562f0d5d28
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-05 00:08:45 -0700

    Pass gap into styles

--- src/components/layouts/grid-layout/grid-layout.tsx
+++ src/components/layouts/grid-layout/grid-layout.tsx
@@ -6,8 +6,20 @@ export const GridLayout = ({ cols, gap, align, justify, children }: GridLayoutPr
 	const mdCols = typeof cols === 'number' ? undefined : cols.md;
 	const lgCols = typeof cols === 'number' ? undefined : cols.lg;
 
+	const isNumericGap = typeof gap === 'number';
+
 	return (
-		<div className={gridLayoutStyles({ cols: baseCols, mdCols, lgCols, align, justify })}>
+		<div
+			className={gridLayoutStyles({
+				cols: baseCols,
+				mdCols,
+				lgCols,
+				align,
+				justify,
+				gap: isNumericGap ? undefined : gap,
+			})}
+			style={isNumericGap ? { gap: `${gap * 4}px` } : undefined}
+		>
 			{children}
 		</div>
 	);
