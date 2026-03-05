20588b99e3501480b5f7e596bef7515597b77f00
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-05 00:38:05 -0700

    Allow users to configure Container props

--- src/components/layouts/layout-component.types.ts
+++ src/components/layouts/layout-component.types.ts
@@ -1,4 +1,5 @@
 import type { BreakpointToken as PandaBreakpointToken } from '@/styled-system/tokens';
+import { ContainerProps } from './container';
 
 type BreakpointToken = PandaBreakpointToken | 'base';
 
@@ -6,5 +7,5 @@ export type ResponsiveValue<T> = T | Partial<Record<BreakpointToken, T>>;
 
 export type LayoutComponentProps = {
 	/** Wraps the layout in a container */
-	container?: boolean;
+	container?: boolean | ContainerProps;
 };
