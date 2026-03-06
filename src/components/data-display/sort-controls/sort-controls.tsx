3c2dc06a5be62d36a52da299f3c3c5628993b758
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-05 23:49:10 -0700

    Create component to sort data

---
+++ src/components/data-display/sort-controls/sort-controls.tsx
@@ -0,0 +1,73 @@
+'use client';
+import { css } from '@/styled-system/css';
+import { SortControlsProps, ActiveSort } from './sort-controls.types';
+import { Button, Icon, IconButton, Text } from '@/components';
+
+const styles = {
+	controlsContainer: css({
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: '1',
+	}),
+};
+
+export const SortControls = <T,>({
+	fields,
+	activeSort,
+	onToggle,
+	onClear,
+	multi,
+}: SortControlsProps<T>) => {
+	const getState = (key: keyof T): ActiveSort<T> | undefined =>
+		activeSort.find((s) => s.key === key);
+
+	return (
+		<div className={styles.controlsContainer}>
+			{fields.map(({ key, label, icon }) => {
+				const state = getState(key);
+				return (
+					<>
+						{icon ? (
+							<IconButton
+								color="tertiary"
+								size="sm"
+								name={icon}
+								key={String(key)}
+								onClick={() => onToggle(key)}
+							>
+								{label}
+								{state && <SortIndicator direction={state.direction} />}
+							</IconButton>
+						) : (
+							<Button color="tertiary" size="sm" onClick={() => onToggle(key)}>
+								{label}
+								{state && <SortIndicator direction={state.direction} />}
+							</Button>
+						)}
+						{/* <button
+							key={String(key)}
+							onClick={() => onToggle(key)}
+							data-active={!!state}
+							data-direction={state?.direction}
+							aria-pressed={!!state}
+							aria-label={`Sort by ${label}${state ? `, currently ${state.direction}ending` : ''}`}
+						>
+							{label}
+							{state && <SortIndicator direction={state.direction} />}
+						</button> */}
+					</>
+				);
+			})}
+
+			{activeSort.length > 0 && (
+				<button onClick={onClear} aria-label="Clear all sorting">
+					<Text size="xs">Clear</Text>
+				</button>
+			)}
+		</div>
+	);
+};
+
+const SortIndicator = ({ direction }: { direction: 'asc' | 'desc' }) =>
+	direction === 'asc' ? <Icon size={8} name="ArrowUp" /> : <Icon size={8} name="ArrowDown" />;
