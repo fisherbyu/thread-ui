e2458dd24f8fe4dac91b8274533e7406e8d41c34
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-06 23:38:47 -0700

    Remove complex override styles

--- src/components/ui/icon-button/icon-button.tsx
+++ src/components/ui/icon-button/icon-button.tsx
@@ -1,32 +1,6 @@
-import { Icon, Button } from '@/components';
 import { IconButtonProps } from './icon-button.types';
-import { getUtilityFontSize, getUtilityIconSize, getUtilitySizeValue } from '@/utils';
-import { CSSProperties } from 'react';
-import { css, cva } from '@/styled-system/css';
-
-const styles = {
-	content: cva({
-		base: {
-			display: 'flex',
-			flexDirection: 'row',
-			whiteSpace: 'nowrap',
-			alignItems: 'center',
-		},
-		variants: {
-			size: {
-				sm: {
-					gap: '0.5',
-				},
-				md: {
-					gap: '1',
-				},
-				lg: {
-					gap: '1.5',
-				},
-			},
-		},
-	}),
-};
+import { Icon, Button } from '@/components';
+import { getUtilityIconSize } from '@/utils';
 
 /**
  * Button with a leading icon and optional text label.
@@ -47,10 +21,6 @@ export const IconButton = ({
 	disabled = false,
 	name,
 }: IconButtonProps) => {
-	const computedStyles: CSSProperties = {
-		marginLeft: `${getUtilitySizeValue(size) - 2}px`,
-		fontSize: getUtilityFontSize(size),
-	};
 	return (
 		<Button
 			fullWidth={fullWidth}
@@ -59,13 +29,10 @@ export const IconButton = ({
 			type={type}
 			margin={margin}
 			disabled={disabled}
+			size={size}
 		>
 			<Icon name={name} size={getUtilityIconSize(size)} />
-			{children && (
-				<div className={styles.content({ size })} style={computedStyles}>
-					{children}
-				</div>
-			)}
+			{children}
 		</Button>
 	);
 };
