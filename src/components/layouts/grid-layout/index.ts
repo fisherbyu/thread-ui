c9c2453cfa0c40cbd32b81d8d8542bf967a48554
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-09 00:21:06 -0600

    Remove Grid Layout

--- src/components/layouts/grid-layout/index.ts
+++
@@ -1,2 +0,0 @@
-export type { GridLayoutProps } from './grid-layout.types'; 
-export { GridLayout } from './grid-layout';
--- src/components/layouts/index.ts
+++ src/components/layouts/index.ts
@@ -1,5 +1,4 @@
 export * from './column-layout';
 export * from './container';
 export * from './footer';
-export * from './grid-layout';
 export * from './masonry-layout';
