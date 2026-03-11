55c2fe8114493b682c7d538638b60a6f454c419a
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-10 19:24:57 -0600

    Export new Inline Controls

--- src/components/data-display/filter-controls/index.ts
+++ src/components/data-display/filter-controls/index.ts
@@ -1,2 +1,3 @@
 export type { FilterControlsProps } from './filter-controls.types';
 export { FilterControls } from './variants/filter-controls';
+export { InlineFilterControls } from './variants/inline-filter-controls';
