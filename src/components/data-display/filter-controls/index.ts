ab88830741f2d45013b0bd5734dad2f371e8cb5a
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-10 19:27:04 -0600

    export filter controls hook

--- src/components/data-display/filter-controls/index.ts
+++ src/components/data-display/filter-controls/index.ts
@@ -1,3 +1,4 @@
+export { useFilterControls } from './use-filter-controls';
 export type { FilterControlsProps } from './filter-controls.types';
 export { FilterControls } from './variants/filter-controls';
 export { InlineFilterControls } from './variants/inline-filter-controls';
