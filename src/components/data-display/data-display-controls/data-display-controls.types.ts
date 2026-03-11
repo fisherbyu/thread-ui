ee3d6a2e6fcbdf93db51cc719bfbaa61a6ad5bd8
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-11 02:32:24 -0600

    Swap filed type

--- src/components/data-display/data-display-controls/data-display-controls.types.ts
+++ src/components/data-display/data-display-controls/data-display-controls.types.ts
@@ -5,7 +5,7 @@ import {
 } from '../sort-controls/sort-controls.types';
 import {
 	FilterControlsConfig,
-	FilterField,
+	ResolvedFilterField,
 	FilterControlsBaseProps,
 } from '../filter-controls/filter-controls.types';
 
@@ -13,7 +13,7 @@ type Override<T, U> = Omit<T, keyof U> & U;
 
 type Prettify<T> = { [K in keyof T]: T[K] } & {};
 
-export type DataField<T> = Override<SortField<T> & FilterField<T>, {}>;
+export type DataField<T> = Override<SortField<T> & ResolvedFilterField<T>, {}>;
 
 export type DataDisplayControlsConfig<T> = Prettify<
 	Override<SortControlsConfig<T> & FilterControlsConfig<T>, { fields: DataField<T>[] }>
@@ -28,5 +28,10 @@ export type DataDisplayControlsProps<T> = Prettify<
 	Omit<
 		SortControlsProps<T> & FilterControlsBaseProps<T>,
 		'fields' | 'hideReset' | 'onToggle' | 'onClear'
-	> & {}
+	> & {
+		fields: DataField<T>[];
+		onToggleSort: SortControlsProps<T>['onToggle'];
+		onClearFilter: FilterControlsBaseProps<T>['onClear'];
+		onToggleFilter: FilterControlsBaseProps<T>['onToggle'];
+	}
 >;
