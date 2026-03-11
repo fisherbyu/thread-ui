c40f0b6dff6edeb162608652bf50b5fa617642ab
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-11 02:32:14 -0600

    Build Data Display Controls

--- src/components/data-display/data-display-controls/data-display-controls.tsx
+++ src/components/data-display/data-display-controls/data-display-controls.tsx
@@ -1,5 +1,46 @@
+import { FilterControls } from '../filter-controls';
+import { SortControls } from '../sort-controls';
 import { DataDisplayControlsProps } from './data-display-controls.types';
 
-export const DataDisplayControls = ({}: DataDisplayControlsProps) => {
-	return <></>;
+export const DataDisplayControls = <T,>({
+	fields,
+	activeSort,
+	activeFilters,
+	onClearAll,
+	isDefault,
+	isActive,
+	onClearFilter,
+	onToggleFilter,
+	onToggleSort,
+	size,
+	color,
+}: DataDisplayControlsProps<T>) => {
+	const sharedProps = {
+		color,
+		size,
+	};
+	const filterProps = {
+		isDefault,
+		fields: fields.map((f) => ({ ...f })),
+		activeFilters,
+		onClear: onClearFilter,
+		onClearAll,
+		onToggle: onToggleFilter,
+		hideReset: true,
+		isActive,
+	};
+
+	const sortProps = {
+		fields,
+		activeSort,
+		onToggle: onToggleSort,
+		onClear: onClearAll,
+		isDefault,
+	};
+	return (
+		<div>
+			<FilterControls {...sharedProps} {...filterProps} />
+			<SortControls {...sharedProps} {...sortProps} />
+		</div>
+	);
 };
