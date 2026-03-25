f32c7fe70b2d4369aa801237fefaf2888a689f5f
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-24 23:07:42 -0600

    Resolve layouts, hide icon when not rendered

--- src/components/media/info-card/info-card.tsx
+++ src/components/media/info-card/info-card.tsx
@@ -1,6 +1,6 @@
 import { Icon } from '@/components/ui';
 import { InfoCardProps } from './info-card.types';
-import { css } from '@/styled-system/css';
+import { css, cx } from '@/styled-system/css';
 import { Text } from '@/components/typography';
 
 const styles = {
@@ -18,32 +18,33 @@ const styles = {
 		width: '100%',
 		backgroundColor: { base: 'background', _hover: 'surface' },
 	}),
+	cardContent: css({
+		display: 'flex',
+		flexDirection: 'column',
+	}),
 	link: css({
 		cursor: 'pointer',
 	}),
 	imageWrapper: css({
 		overflow: 'hidden',
 		width: '100%',
-		height: '83.333333%',
+		flex: '1',
+		minHeight: '0',
 	}),
 	image: css({
 		height: '100%',
 		width: '100%',
 		objectFit: 'cover',
 	}),
-	captionBlock: css({
+	caption: css({
 		padding: '2.5',
 		height: '10',
-	}),
-	caption: css({
 		display: 'flex',
 		gap: '2',
 		justifyContent: 'flex-start',
 		flexDirection: 'row',
 		alignItems: 'center',
-	}),
-	captionIcon: css({
-		paddingRight: '0.25rem',
+		flexShrink: 0,
 	}),
 };
 
@@ -65,35 +66,29 @@ export const InfoCard = ({ title, url, icon, img }: InfoCardProps) => {
 		} else if (icon.type === 'svg') {
 			return (
 				<img
-					className={styles.captionIcon}
-					height={23}
-					width={23}
+					height={24}
+					width={24}
 					src={icon.content}
 					alt="Article Icon"
+					onError={(e) => {
+						e.currentTarget.style.display = 'none';
+					}}
 				/>
 			);
-		} else {
-			<span className={styles.captionIcon} role="img">
-				{JSON.parse(`"${icon.content}"`)}
-			</span>;
 		}
 	};
 
 	return (
-		<div className={styles.card}>
-			<a href={url} className={styles.link}>
-				<div className={styles.imageWrapper}>
-					<img className={styles.image} src={img} alt="Article Cover Image" />
-				</div>
-				<div className={styles.captionBlock}>
-					<span className={styles.caption}>
-						{renderIcon()}
-						<Text size="sm" truncate inline>
-							{title}
-						</Text>
-					</span>
-				</div>
-			</a>
-		</div>
+		<a href={url} className={cx(styles.cardContent, styles.link, styles.card)}>
+			<div className={styles.imageWrapper}>
+				<img className={styles.image} src={img} alt="Article Cover Image" />
+			</div>
+			<div className={styles.caption}>
+				{renderIcon()}
+				<Text size="sm" truncate inline>
+					{title}
+				</Text>
+			</div>
+		</a>
 	);
 };
