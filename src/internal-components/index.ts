3bb2dc4375fc87c52f54db22b36eccd775e44004
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-29 17:51:49 -0600

    Create and export dynamic icon

--- src/internal-components/index.ts
+++ src/internal-components/index.ts
@@ -1,5 +1,6 @@
 // Export Internal Components
 export * from './conditional-wrapper';
+export * from './dynamic-icon';
 export * from './image';
 export * from './link-wrapper';
 export * from './optional-icon-button';
