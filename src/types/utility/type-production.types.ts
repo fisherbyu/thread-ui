6e57d30a26d3fc15d87479c223edc25b6984e03a
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-31 22:37:46 -0600

    Create OptionalKey utility type

--- src/types/utility/type-production.types.ts
+++ src/types/utility/type-production.types.ts
@@ -1,3 +1,13 @@
 export type Override<T, U> = Omit<T, keyof U> & U;
 
 export type Prettify<T> = { [K in keyof T]: T[K] } & {};
+
+export type OptionalKey<T, K extends string> = T extends (infer U)[]
+	? OptionalKey<U, K>[]
+	: T extends object
+		? Prettify<
+				{ [P in Exclude<keyof T, K>]: OptionalKey<T[P], K> } & {
+					[P in Extract<keyof T, K>]?: T[P];
+				}
+			>
+		: T;
