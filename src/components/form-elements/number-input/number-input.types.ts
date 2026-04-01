a8b4bc45a0303dfd7dd1c5cc90fd8feef3c1ec0c
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-31 20:59:09 -0600

    Allow for null values

--- src/components/form-elements/number-input/number-input.types.ts
+++ src/components/form-elements/number-input/number-input.types.ts
@@ -2,7 +2,7 @@ import { Prettify } from '@/types';
 import { InputProps } from '../input-props.types';
 
 export type NumberInputProps = Prettify<
-	InputProps<number> & {
+	InputProps<number | null> & {
 		/** Applies dark styling */
 		dark?: boolean;
 		/** Minimum allowed value */
