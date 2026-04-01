f35230c02d73889798b706810abac454ffd24296
Andrew Fisher <fisherandrew777@gmail.com>
2026-03-31 19:39:03 -0600

    Make properties more visible

--- src/components/form-elements/number-input/number-input.types.ts
+++ src/components/form-elements/number-input/number-input.types.ts
@@ -1,10 +1,13 @@
+import { Prettify } from '@/types';
 import { InputProps } from '../input-props.types';
 
-export type NumberInputProps = InputProps<number> & {
-	/** Applies dark styling */
-	dark?: boolean;
-	/** Minimum allowed value */
-	min?: number;
-	/** Maximum allowed value */
-	max?: number;
-};
+export type NumberInputProps = Prettify<
+	InputProps<number> & {
+		/** Applies dark styling */
+		dark?: boolean;
+		/** Minimum allowed value */
+		min?: number;
+		/** Maximum allowed value */
+		max?: number;
+	}
+>;
