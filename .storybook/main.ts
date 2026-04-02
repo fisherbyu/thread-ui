1949b79c603da32772dce809d9d9ee5e4be588b4
Andrew Fisher <fisherandrew777@gmail.com>
2026-04-02 00:28:58 -0600

    force title and icon override

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,36 @@ const config: StorybookConfig = {
 		name: '@storybook/react-vite',
 		options: {},
 	},
+	managerHead: (head) => `
+        ${head}
+        <script>
+            // Title override
+            new MutationObserver(function() {
+                if (document.title !== 'Thread UI') {
+                    document.title = 'Thread UI';
+                }
+            }).observe(document.querySelector('title'), {
+                childList: true,
+                subtree: true,
+                characterData: true
+            });
+            // Favicon override
+            new MutationObserver(function() {
+                var icons = document.querySelectorAll("link[rel='icon'], link[rel*='icon']");
+                icons.forEach(function(icon) {
+                    if (icon.href.indexOf('icon.ico') === -1) {
+                        icon.href = '/icon.ico';
+                        icon.type = 'image/x-icon';
+                    }
+                });
+            }).observe(document.head, {
+                childList: true,
+                subtree: true,
+                attributes: true,
+                attributeFilter: ['href']
+            });
+        </script>
+    `,
 	typescript: { check: true, reactDocgen: 'react-docgen-typescript' },
 	async viteFinal(config) {
 		const { mergeConfig } = await import('vite');
