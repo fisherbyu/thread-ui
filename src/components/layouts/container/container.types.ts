6642c49bcb7f7ee72d6d66b3d300e94fe2a1bd01
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-13 00:23:40 -0600

    Update types

--- src/components/layouts/container/container.types.ts
+++ src/components/layouts/container/container.types.ts
@@ -1,11 +1,11 @@
-import { BgColorOptions } from '@/types';
+import { BgColorOptions, Prettify } from '@/types';
 import { JSX, ReactNode } from 'react';
 
-type BlockElements = Pick<JSX.IntrinsicElements, 'div' | 'section'>;
+type BlockElements = Pick<JSX.IntrinsicElements, 'div' | 'section' | 'footer'>;
 
 export type ContainerProps = {
 	/** HTML element to render as @default `'div'` */
-	as?: keyof BlockElements;
+	as?: Prettify<keyof BlockElements>;
 	/** Background color token @default `'none'` */
 	bg?: BgColorOptions;
 	/** Container Contents */
