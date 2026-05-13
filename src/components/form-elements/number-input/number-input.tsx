2a61ef60f8163c232d1b129f96dc7ece27388978
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-13 16:08:52 -0600

    Remove dark

--- src/components/form-elements/number-input/number-input.tsx
+++ src/components/form-elements/number-input/number-input.tsx
@@ -93,7 +93,6 @@ export const NumberInput = ({
 	value,
 	placeholder,
 	required,
-	dark,
 	min,
 	max,
 	onChange,
--- src/components/form-elements/number-input/number-input.types.ts
+++ src/components/form-elements/number-input/number-input.types.ts
@@ -3,8 +3,6 @@ import { InputProps } from '../input-props.types';
 
 export type NumberInputProps = Prettify<
 	InputProps<number | null> & {
-		/** Applies dark styling */
-		dark?: boolean;
 		/** Minimum allowed value */
 		min?: number;
 		/** Maximum allowed value */
