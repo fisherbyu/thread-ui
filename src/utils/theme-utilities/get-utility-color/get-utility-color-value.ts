50649735c60589532f03ca2b533a725afbf1d0fa
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-13 17:34:21 -0600

    Refactor to accept shade prop

--- src/utils/theme-utilities/get-utility-color/get-utility-color-value.ts
+++ src/utils/theme-utilities/get-utility-color/get-utility-color-value.ts
@@ -1,36 +1,44 @@
 import { ThreadTheme } from '@/theme';
-import { UtilityColorOptions } from '../../../types';
+import { ColorShadeOptions, UtilityColorOptions } from '@/types';
+
+const getUtilityColorObject = (color: UtilityColorOptions) => {
+	switch (color) {
+		case 'primary':
+			return ThreadTheme.primary;
+		case 'secondary':
+			return ThreadTheme.secondary;
+		case 'tertiary':
+			return ThreadTheme.tertiary;
+		case 'black':
+			return { main: ThreadTheme.black } as Record<string, string>;
+		case 'gray':
+			return ThreadTheme.gray;
+		case 'success':
+			return ThreadTheme.success;
+		case 'error':
+			return ThreadTheme.error;
+		case 'warning':
+			return ThreadTheme.warning;
+		case 'info':
+			return ThreadTheme.info;
+		case 'text':
+			return { main: ThreadTheme.text.standard } as Record<string, string>;
+		default: {
+			const _exhaustive: never = color;
+			return ThreadTheme.primary;
+		}
+	}
+};
 
 /**
  * Returns corresponding default theme color CSS Variable
  * @param color Color Option
+ * @param shade Optional shade (defaults to 'main'). 'black' and 'text' only support 'main'.
  * @returns CSS Theme Variable string
  */
-export const getUtilityColorValue = (color: UtilityColorOptions): string => {
-	switch (color) {
-		case 'primary':
-			return ThreadTheme.primary.main;
-		case 'secondary':
-			return ThreadTheme.secondary.main;
-		case 'tertiary':
-			return ThreadTheme.tertiary.main;
-		case 'black':
-			return ThreadTheme.black;
-		case 'gray':
-			return ThreadTheme.gray.main;
-		case 'success':
-			return ThreadTheme.success.main;
-		case 'error':
-			return ThreadTheme.error.main;
-		case 'warning':
-			return ThreadTheme.warning.main;
-		case 'info':
-			return ThreadTheme.info.main;
-		case 'text':
-			return ThreadTheme.text.standard;
-		default: {
-			const _exhaustive: never = color;
-			return ThreadTheme.primary.main;
-		}
-	}
+export const getUtilityColorValue = (
+	color: UtilityColorOptions,
+	shade?: ColorShadeOptions
+): string => {
+	return getUtilityColorObject(color)[shade ?? 'main'];
 };
