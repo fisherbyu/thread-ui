e1e937bb5bf19cd852a04714a3872d5aafc736ec
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-16 16:24:23 -0600

    Update options

--- src/types/theme/theme.types.ts
+++ src/types/theme/theme.types.ts
@@ -182,7 +182,7 @@ export type HeadingFontSizeOptions = Prettify<keyof HeadingFontSizes>;
 export type SpacingScale = Prettify<{ none: string } & FullUtilitySizes>;
 
 /** Em-based margin ratios for typography roles */
-export type SpacingScaleOptions = '0' | '0.5em' | '0.75em' | '1em';
+export type SpacingScaleOptions = Prettify<keyof SpacingScale>;
 
 /** Combined font size scales */
 export type ThemeFontSizes = Prettify<{
