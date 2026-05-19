02e745f181df64c61f9ea5f6d12c70bdb2524f54
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-19 13:38:29 -0600

    Update exports

--- src/components/typography/index.ts
+++ src/components/typography/index.ts
@@ -1,4 +1,26 @@
-export type { TypographyProps, BodyTextProps as TextProps } from './typography';
-export { Title, H1, H2, H3, Text, Subtitle, List, OrderedList, Code } from './typography';
+export type {
+	BaseTypographyPresentationProps,
+	ExpandedTypographyPresentationProps,
+	TypographyProps,
+} from './typography.types';
+
+export type { TypographyHeadingProps, SubtitleProps } from './heading/heading.types';
+
+export type {
+	TypographyBodyTypes,
+	TextProps,
+	ListProps,
+	OrderedListProps,
+} from './body/body.types';
+
+export type { CodeProps } from './code/code.types';
 
 export * from './page-header';
+
+export { Title, H1, H2, H3, Subtitle } from './heading/heading';
+
+export { Text } from './body/text';
+
+export { List, OrderedList } from './body/list';
+
+export { Code } from './code/code';
