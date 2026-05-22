3abcb9c7be72b7afa710f59fce0dbdcbbbb4dc11
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-22 02:09:10 -0600

    Locate default

--- src/components/ui/tab-view/tab-view.tsx
+++ src/components/ui/tab-view/tab-view.tsx
@@ -5,8 +5,10 @@ import { TabViewContent } from './components/tab-view-content';
 import { TabViewProvider } from './tab-view-context';
 
 export const TabView = ({ items, defaultValue, layer = 'surface' }: TabViewProps) => {
+	const defaultItem = items.find((item) => item.name === defaultValue) ?? items[0];
+
 	return (
-		<TabViewProvider value={{ activeValue: defaultValue, items, layer }}>
+		<TabViewProvider value={{ activeItem: defaultItem, items, layer }}>
 			<TabViewContent />
 		</TabViewProvider>
 	);
