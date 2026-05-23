209510a0c737eb5bbf03cd850779266ba3d86add
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-23 01:04:39 -0600

    Memoize calculations, guard null case

--- src/components/ui/tab-view/tab-view.tsx
+++ src/components/ui/tab-view/tab-view.tsx
@@ -1,14 +1,27 @@
 'use client';
-
-import { TabViewProps } from './tab-view.types';
+import { useMemo } from 'react';
+import { TabViewProps, TabViewState } from './tab-view.types';
 import { TabViewContent } from './components/tab-view-content';
 import { TabViewProvider } from './tab-view-context';
 
-export const TabView = ({ items, defaultValue, layer = 'surface' }: TabViewProps) => {
-	const defaultItem = items.find((item) => item.name === defaultValue) ?? items[0];
+export const TabView = ({ items, defaultValueId, layer = 'surface' }: TabViewProps) => {
+	const value = useMemo<TabViewState>(() => {
+		const itemsStore = Object.fromEntries(
+			items.map((item) => [item.id, item])
+		) as TabViewState['items'];
+
+		const itemOrder = items.map((item) => item.id);
+
+		const activeItemId =
+			defaultValueId && itemsStore[defaultValueId] ? defaultValueId : items[0]?.id;
+
+		return { items: itemsStore, itemOrder, activeItemId, layer };
+	}, [items, defaultValueId, layer]);
+
+	if (!items.length) return null;
 
 	return (
-		<TabViewProvider value={{ activeItem: defaultItem, items, layer }}>
+		<TabViewProvider value={value}>
 			<TabViewContent />
 		</TabViewProvider>
 	);
