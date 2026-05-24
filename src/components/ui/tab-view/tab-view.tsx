70b3d72ee2c5a2747cc9a8c5f206332c39916cfd
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-24 17:07:14 -0600

    Rename list

--- src/components/ui/tab-view/tab-view.tsx
+++ src/components/ui/tab-view/tab-view.tsx
@@ -4,21 +4,21 @@ import { TabViewContent } from './components/tab-view-content';
 import { TabViewProvider } from './tab-view-context';
 
 export const TabView = ({
-	items,
+	items: itemList,
 	defaultValueId,
 	layer = 'surface',
 	unmountInactive = false,
 }: TabViewProps) => {
-	if (!items.length) return null;
+	if (!itemList.length) return null;
 
 	const itemsStore = Object.fromEntries(
-		items.map((item) => [item.id, item])
+		itemList.map((item) => [item.id, item])
 	) as TabViewState['items'];
 
-	const itemOrder = items.map((item) => item.id);
+	const itemOrder = itemList.map((item) => item.id);
 
 	const activeItemId =
-		defaultValueId && itemsStore[defaultValueId] ? defaultValueId : items[0]?.id;
+		defaultValueId && itemsStore[defaultValueId] ? defaultValueId : itemList[0]?.id;
 
 	const initialValue: TabViewState = {
 		items: itemsStore,
