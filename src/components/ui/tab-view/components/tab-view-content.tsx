cab5cd3a324b7dd2486ba62e2ba975133c76e235
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-24 01:23:45 -0600

    Render all items, manage display through CSS

--- src/components/ui/tab-view/components/tab-view-content.tsx
+++ src/components/ui/tab-view/components/tab-view-content.tsx
@@ -1,11 +1,17 @@
 import { TabViewControls } from './tab-view-controls';
 import { TabItemContent } from './tab-item-content';
+import { useTabViewContext } from '../tab-view-context';
 
 export const TabViewContent = () => {
+	const {
+		value: { items },
+	} = useTabViewContext();
 	return (
 		<div>
 			<TabViewControls />
-			<TabItemContent />
+			{Object.entries(items).map(([itemId]) => (
+				<TabItemContent key={itemId} itemId={itemId} />
+			))}
 		</div>
 	);
 };
