35cd46ae1b39ad8c06c07b39a26fa0dbce871761
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-24 17:45:12 -0600

    Move story into UI section

--- src/components/ui/tab-view/tab-view.stories.tsx
+++ src/components/ui/tab-view/tab-view.stories.tsx
@@ -5,7 +5,7 @@ import { Text } from '../../typography';
 import { Container } from '../../layouts/container/container';
 
 const meta: Meta<typeof TabView> = {
-	title: 'Components/TabView',
+	title: 'UI Components/TabView',
 	component: TabView,
 	parameters: {
 		layout: 'fullscreen',
