612de6dc330321e6a2cdd58d81ebd95485da8495
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-25 23:17:53 -0600

    Display Items

--- src/components/ui/carousel/components/carousel-content.tsx
+++ src/components/ui/carousel/components/carousel-content.tsx
@@ -1,3 +1,17 @@
+'use client';
+import { Container } from '@/components/layouts';
+import { useCarouselContext } from '../carousel-context';
+import { ItemDisplay } from './item-display';
+
 export const CarouselContent = () => {
-	return <></>;
+	const {
+		value: { itemOrder },
+	} = useCarouselContext();
+	return (
+		<Container>
+			{itemOrder.map((itemId) => (
+				<ItemDisplay itemId={itemId} />
+			))}
+		</Container>
+	);
 };
