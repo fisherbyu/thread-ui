69cbb743a416b390936fcfc4a23a448ab6a7309d
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-25 23:44:33 -0600

    Add header

--- src/components/ui/carousel/components/carousel-content.tsx
+++ src/components/ui/carousel/components/carousel-content.tsx
@@ -2,13 +2,21 @@
 import { Container } from '@/components/layouts';
 import { useCarouselContext } from '../carousel-context';
 import { ItemDisplay } from './item-display';
+import { css } from '@/styled-system/css';
+import { CarouselHeader } from './carousel-header';
+
+const styles = {
+	content: css({}),
+};
 
 export const CarouselContent = () => {
 	const {
 		value: { itemOrder },
 	} = useCarouselContext();
+
 	return (
 		<Container>
+			<CarouselHeader />
 			{itemOrder.map((itemId) => (
 				<ItemDisplay itemId={itemId} />
 			))}
