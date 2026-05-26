79b68796aa95c53b2fb279862c36e48f4c9bfe2b
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-25 23:35:04 -0600

    Add controls positioning

--- src/components/ui/carousel/carousel.types.ts
+++ src/components/ui/carousel/carousel.types.ts
@@ -1,6 +1,8 @@
 import { Prettify } from '@/types';
 import { ReactNode } from 'react';
 
+export type CarouselControlsPosition = 'above' | 'around' | 'below';
+
 /** A single pane consumed and displayed by the `Carousel` component */
 export type CarouselItem = {
 	/** Optional title rendered with content */
@@ -18,10 +20,12 @@ export type CarouselProps = Prettify<{
 	title?: string | ReactNode;
 	/** Items to render, in display order */
 	items: InternalCarouselItem[];
+	/** Location to Render Controls @default 'around' */
+	controlsPosition?: CarouselControlsPosition;
 }>;
 
 export type CarouselState = Prettify<
-	Pick<CarouselProps, 'title'> & {
+	Pick<CarouselProps, 'title' | 'controlsPosition'> & {
 		items: Record<CarouselItemId, InternalCarouselItem>;
 		itemOrder: CarouselItemId[];
 		activeItemId: CarouselItemId;
