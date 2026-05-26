83c52365cb96d9e180807f398b634c41c29276b9
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-25 23:07:11 -0600

    Pass props and state into context

--- src/components/ui/carousel/carousel.tsx
+++ src/components/ui/carousel/carousel.tsx
@@ -1,5 +1,34 @@
-import { CarouselProps } from './carousel.types';
+'use client';
+import { CarouselProvider } from './carousel-context';
+import { CarouselProps, CarouselState } from './carousel.types';
+import { CarouselContent } from './components/carousel-content';
 
-export const Carousel = ({}: CarouselProps) => {
-	return <></>;
+export const Carousel = ({ title, items: itemList }: CarouselProps) => {
+	if (!itemList.length) return null;
+
+	const normalizedItems = itemList.map((item, index) => ({
+		...item,
+		id: String(index),
+	}));
+
+	const items = Object.fromEntries(
+		normalizedItems.map((item) => [item.id, item])
+	) as CarouselState['items'];
+
+	const itemOrder = normalizedItems.map((item) => item.id);
+
+	const activeItemId = normalizedItems[0].id;
+
+	const initialState = {
+		title,
+		items,
+		itemOrder,
+		activeItemId,
+	};
+
+	return (
+		<CarouselProvider initialValue={initialState}>
+			<CarouselContent />
+		</CarouselProvider>
+	);
 };
