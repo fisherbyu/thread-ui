999583a640febed819975a9e6bdfb493c4bfeccb
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-25 23:52:44 -0600

    Allow display of controls

--- src/components/ui/carousel/components/carousel-content.tsx
+++ src/components/ui/carousel/components/carousel-content.tsx
@@ -4,22 +4,38 @@ import { useCarouselContext } from '../carousel-context';
 import { ItemDisplay } from './item-display';
 import { css } from '@/styled-system/css';
 import { CarouselHeader } from './carousel-header';
+import { CarouselControls } from './carousel-controls';
 
 const styles = {
-	content: css({}),
+	content: css({
+		display: 'flex',
+		flexDirection: 'column',
+	}),
+	controls: css({
+		marginX: 'auto',
+	}),
 };
 
 export const CarouselContent = () => {
 	const {
-		value: { itemOrder },
+		value: { itemOrder, controlsPosition },
 	} = useCarouselContext();
 
 	return (
 		<Container>
-			<CarouselHeader />
-			{itemOrder.map((itemId) => (
-				<ItemDisplay itemId={itemId} />
-			))}
+			<div className={styles.content}>
+				<CarouselHeader />
+				<div>
+					{itemOrder.map((itemId) => (
+						<ItemDisplay itemId={itemId} />
+					))}
+				</div>
+			</div>
+			{controlsPosition === 'below' && (
+				<div className={styles.controls}>
+					<CarouselControls />
+				</div>
+			)}
 		</Container>
 	);
 };
