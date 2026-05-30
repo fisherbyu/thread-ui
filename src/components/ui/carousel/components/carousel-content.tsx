dcce119dd6e402a80187c121f08dc61d22162fa3
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-30 01:09:58 -0600

    Wrap contents in embla

--- src/components/ui/carousel/components/carousel-content.tsx
+++ src/components/ui/carousel/components/carousel-content.tsx
@@ -1,11 +1,10 @@
 'use client';
-import { Container } from '@/components/layouts';
-import { useCarouselContext } from '../carousel-context';
 import { css } from '@/styled-system/css';
+import useEmblaCarousel from 'embla-carousel-react';
+import { useCarouselContext } from '../carousel-context';
 import { CarouselHeader } from './carousel-header';
-import { CarouselControls } from './carousel-controls';
 import { CarouselItem } from './carousel-item';
-import { ColumnSkeleton } from '@/components/layouts/column-layout/column-skeleton';
+import { Container } from '@/components/layouts';
 
 const styles = {
 	content: css({
@@ -20,8 +19,8 @@ const styles = {
 	viewport: css({ width: '100%', overflow: 'hidden' }),
 	track: css({
 		display: 'flex',
-		flexDirection: 'row',
-		translateX: '',
+		gap: { sm: '1', md: '2' },
+		touchAction: 'pan-y pinch-zoom',
 	}),
 };
 
@@ -30,14 +29,18 @@ export const CarouselContent = () => {
 		value: { itemOrder },
 	} = useCarouselContext();
 
+	const [emblaRef] = useEmblaCarousel();
+
 	return (
 		<Container>
 			<CarouselHeader />
-			<ColumnSkeleton mdcol={3} lgcol={3}>
-				{itemOrder.map((itemId) => (
-					<CarouselItem itemId={itemId} />
-				))}
-			</ColumnSkeleton>
+			<div ref={emblaRef} className={styles.viewport}>
+				<div className={styles.track}>
+					{itemOrder.map((itemId) => (
+						<CarouselItem key={itemId} itemId={itemId} />
+					))}
+				</div>
+			</div>
 		</Container>
 	);
 };
