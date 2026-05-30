b05b9b295abd693e15f85fdd74d47127bfb805fb
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-29 23:05:35 -0600

    update indexing and column controls

--- src/components/ui/carousel/carousel.types.ts
+++ src/components/ui/carousel/carousel.types.ts
@@ -1,3 +1,5 @@
+import { ColumnSkeletonProps } from '@/components/layouts/column-layout/column-skeleton';
+import { LargeColumnOptions, MediumColumnOptions } from '@/components';
 import { Prettify } from '@/types';
 import { ComponentType, ReactElement, ReactNode } from 'react';
 
@@ -24,14 +26,17 @@ export type CarouselProps = Prettify<{
 	controlsPosition?: CarouselControlsPosition;
 	/** Carousel Item Wrapper @default `Card` */
 	itemWrapper: ComponentType<any>;
+	mdCols: MediumColumnOptions;
+	lgCol?: LargeColumnOptions;
 }>;
 
 export type CarouselState = Prettify<
-	Pick<CarouselProps, 'title'> & {
-		items: Record<CarouselItemId, InternalCarouselItem>;
-		itemOrder: CarouselItemId[];
-		activeItemId: CarouselItemId;
-		controlsPosition: CarouselControlsPosition;
-		ItemWrapper: CarouselProps['itemWrapper'];
-	}
+	Pick<CarouselProps, 'title' | 'mdCols'> &
+		Required<Pick<CarouselProps, 'lgCol'>> & {
+			items: Record<CarouselItemId, InternalCarouselItem>;
+			itemOrder: CarouselItemId[];
+			current: number;
+			controlsPosition: CarouselControlsPosition;
+			ItemWrapper: CarouselProps['itemWrapper'];
+		}
 >;
