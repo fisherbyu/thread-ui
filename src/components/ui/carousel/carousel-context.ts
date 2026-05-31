ece3befbe804dfb65cb7a4c70f8216ad695e9a6a
Andrew Fisher <fisherandrew777@gmail.com>
2026-05-31 00:48:20 -0600

    Create new embla wrapper

--- src/components/ui/carousel/carousel-context.ts
+++ src/components/ui/carousel/carousel-context.ts
@@ -1,6 +1,9 @@
 'use client';
-import { createStatefulComponentContext } from '@/utils';
-import { CarouselState } from './carousel.types';
+import { createStatefulComponentContext, createComponentContext } from '@/utils';
+import { CarouselState, CarouselEmblaContext } from './carousel.types';
 
 export const [CarouselProvider, useCarouselContext] =
 	createStatefulComponentContext<CarouselState>('Carousel');
+
+export const [CarouselEmblaProvider, useCarouselEmblaContext] =
+	createComponentContext<CarouselEmblaContext>('CarouselEmbla');
