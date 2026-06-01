a424e0378dbb21ddbf2bca5497996f9af0d0395b
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-01 01:52:22 -0600

    Begin create track item

---
+++ src/components/ui/gallery/components/item-track/track-item.tsx
@@ -0,0 +1,27 @@
+'use client';
+import { css } from '@/styled-system/css';
+import { useGalleryContext, useGalleryEmblaContext } from '../../gallery-context';
+import { GalleryItemId } from '../../gallery.types';
+
+type TrackItemProps = {
+	itemId: GalleryItemId;
+};
+
+const styles = {
+	container: css({
+		borderWidth: 'md',
+		borderColor: 'transparent',
+		cursor: 'pointer',
+	}),
+};
+
+export const TrackItem = ({ itemId }: TrackItemProps) => {
+	const {
+		value: { items },
+	} = useGalleryContext();
+	const { onTrackItemClick } = useGalleryEmblaContext();
+
+	const item = items[itemId];
+
+	return <button>{item.content}</button>;
+};
