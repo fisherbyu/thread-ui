7397bf6dd97b4e764f91b6e62980a63b16d3e0ee
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-01 01:56:13 -0600

    Create item track

---
+++ src/components/ui/gallery/components/item-track/item-track.tsx
@@ -0,0 +1,34 @@
+'use client';
+import { css } from '@/styled-system/css';
+import { useGalleryContext, useGalleryEmblaContext } from '../../gallery-context';
+import { TrackItem } from './track-item';
+
+const styles = {
+	viewport: css({
+		width: '100%',
+		overflow: 'hidden',
+	}),
+	window: css({
+		display: 'flex',
+		flexDirection: 'row',
+		paddingLeft: 'calc(12px * -1)',
+	}),
+};
+
+export const ItemTrack = () => {
+	const {
+		value: { itemOrder },
+	} = useGalleryContext();
+
+	const { emblaItemTrackRef } = useGalleryEmblaContext();
+
+	return (
+		<div ref={emblaItemTrackRef} className={styles.viewport}>
+			<div className={styles.window}>
+				{itemOrder.map((itemId) => (
+					<TrackItem itemId={itemId} />
+				))}
+			</div>
+		</div>
+	);
+};
