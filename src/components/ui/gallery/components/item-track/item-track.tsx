99d967e262c2c825ae14bdcb87ee4070754bce23
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-01 23:37:40 -0600

    Fix margin

--- src/components/ui/gallery/components/item-track/item-track.tsx
+++ src/components/ui/gallery/components/item-track/item-track.tsx
@@ -10,7 +10,7 @@ const styles = {
 	window: css({
 		display: 'flex',
 		flexDirection: 'row',
-		paddingLeft: 'calc(var(--thread-gallery-item-track-spacing) * -1)', // Tracks with GALLERY_DISPLAY_CONSTANT_NAMES
+		marginLeft: 'calc(var(--thread-gallery-item-track-spacing) * -1)', // Tracks with GALLERY_DISPLAY_CONSTANT_NAMES
 	}),
 };
 
