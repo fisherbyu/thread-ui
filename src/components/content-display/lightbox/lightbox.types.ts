761ada3e875c7f9c00a956d6e59a0d79b46bb1fb
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-03 20:33:16 -0600

    Update types

--- src/components/content-display/lightbox/lightbox.types.ts
+++ src/components/content-display/lightbox/lightbox.types.ts
@@ -2,4 +2,6 @@ import { Prettify } from '@/types';
 import { GalleryProps } from '../gallery';
 import { ModalProps } from '@/components/ui';
 
-export type LightboxProps = Prettify<GalleryProps & Pick<ModalProps, 'isOpen' | 'onClose'>>;
+export type LightboxProps = Prettify<
+	Omit<GalleryProps, 'size'> & Pick<ModalProps, 'isOpen' | 'onClose'>
+>;
