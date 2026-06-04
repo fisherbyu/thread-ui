8e5896cc087418302e6922102251c9a602b1edc5
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-03 20:35:22 -0600

    Create state type

--- src/components/content-display/lightbox/lightbox.types.ts
+++ src/components/content-display/lightbox/lightbox.types.ts
@@ -5,3 +5,5 @@ import { ModalProps } from '@/components/ui';
 export type LightboxProps = Prettify<
 	Omit<GalleryProps, 'size'> & Pick<ModalProps, 'isOpen' | 'onClose'>
 >;
+
+export type LightboxState = LightboxProps;
