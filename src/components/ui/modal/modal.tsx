87037ed7774cfa21a73fdebb782157a026f1c643
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-03 19:45:42 -0600

    Update import

--- src/components/ui/modal/modal.tsx
+++ src/components/ui/modal/modal.tsx
@@ -3,7 +3,7 @@ import { ModalProps } from './modal.types';
 import { ModalProvider } from './modal-context';
 import { ModalContent } from './components/modal-content';
 import { cva } from '@/styled-system/css';
-import { ModalPrimitive } from '@/internal-components/modal-primitive';
+import { ModalPrimitive } from '@/internal-components';
 
 const styles = {
 	overlay: cva({
