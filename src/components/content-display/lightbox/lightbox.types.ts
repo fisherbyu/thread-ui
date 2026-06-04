03d8cc076aca64c7088921f54d4233a4e8cda58e
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-03 20:30:24 -0600

    Define lightbox props

--- src/components/content-display/lightbox/lightbox.types.ts
+++ src/components/content-display/lightbox/lightbox.types.ts
@@ -1,1 +1,7 @@
-export type LightboxProps = {};
+import { Prettify } from '@/types';
+import { GalleryProps } from '../gallery';
+import { ModalProps } from '@/components/ui';
+
+export type LightboxProps = Prettify<
+	GalleryProps & Pick<ModalProps, 'isOpen' | 'onClose' | 'title'>
+>;
