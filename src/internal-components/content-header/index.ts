a3db8668e8172fba000b4656c7316a541f5f2292
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-14 00:33:23 -0600

    Update export

--- src/internal-components/content-header/index.ts
+++ src/internal-components/content-header/index.ts
@@ -1,2 +1,2 @@
-export type { ContentHeaderProps } from './content-header.types'; 
+export type { ContentHeaderProps } from './content-header.types';
 export { ContentHeader } from './content-header';
