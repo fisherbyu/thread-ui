8c3369762121c18c982b10b630ce7aab6088b63d
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-13 21:24:46 -0600

    Create content header types

---
+++ src/internal-components/content-header/content-header.types.ts
@@ -0,0 +1,23 @@
+import { ReactElement, ReactNode } from 'react';
+import { Title, H1, H2, H3, Subtitle } from '@/components/typography/';
+import {
+	TypographyHeadingProps,
+	SubtitleProps,
+} from '@/components/typography/heading/heading.types';
+
+type HeadingElement = ReactElement<
+	TypographyHeadingProps,
+	typeof Title | typeof H1 | typeof H2 | typeof H3
+>;
+
+type SubtitleElement = ReactElement<SubtitleProps, typeof Subtitle>;
+
+type TitleType = string | HeadingElement;
+type SubtitleType = string | SubtitleElement;
+
+export type ContentTitle = {
+	title: TitleType;
+	subtitle?: SubtitleType;
+};
+
+export type ContentHeaderProps = ContentTitle & { headerRight?: ReactNode };
