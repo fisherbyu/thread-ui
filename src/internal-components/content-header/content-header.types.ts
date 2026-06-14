3fdafc13a7cdf34b027806d717ef9e76d03dbfa6
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-13 21:26:52 -0600

    Prettify types

--- src/internal-components/content-header/content-header.types.ts
+++ src/internal-components/content-header/content-header.types.ts
@@ -4,6 +4,7 @@ import {
 	TypographyHeadingProps,
 	SubtitleProps,
 } from '@/components/typography/heading/heading.types';
+import { Prettify } from '@/types';
 
 type HeadingElement = ReactElement<
 	TypographyHeadingProps,
@@ -20,4 +21,4 @@ export type ContentTitle = {
 	subtitle?: SubtitleType;
 };
 
-export type ContentHeaderProps = ContentTitle & { headerRight?: ReactNode };
+export type ContentHeaderProps = Prettify<ContentTitle & { headerRight?: ReactNode }>;
