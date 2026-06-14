0da45c59ee98a6e42f23d7a81bf391ce6399a761
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-14 00:37:39 -0600

    Utilize shared title type

--- src/components/content-display/carousel/carousel.types.ts
+++ src/components/content-display/carousel/carousel.types.ts
@@ -3,6 +3,7 @@ import { Prettify } from '@/types';
 import { EmblaViewportRefType } from 'embla-carousel-react';
 import { EmblaCarouselType } from 'embla-carousel';
 import { LargeColumnOptions, MediumColumnOptions } from '@/components';
+import { ContentTitle } from '@/internal-components';
 
 export type CarouselControlsPosition = 'above' | 'around' | 'below' | 'none';
 
@@ -22,23 +23,22 @@ type ItemWrapperComponent = ComponentType<any>;
 
 export type ItemWrapperOptions = 'none' | 'card' | 'cardFrame' | ItemWrapperComponent;
 
-export type CarouselProps = Prettify<{
-	/** Optional Title */
-	title?: string | ReactNode;
-	/** Items to render, in display order */
-	items: CarouselItem[];
-	/** Location to Render Controls @default 'around' */
-	controlsPosition?: CarouselControlsPosition;
-	/** Optional Carousel Item Wrapper @default `cardOutline` */
-	itemWrapper?: ItemWrapperOptions;
-	/** Number of Columns in Medium Viewport */
-	mdCols: MediumColumnOptions;
-	/** Number of Columns in Large Viewport */
-	lgCols?: LargeColumnOptions;
-}>;
+export type CarouselProps = ContentTitle &
+	Prettify<{
+		/** Items to render, in display order */
+		items: CarouselItem[];
+		/** Location to Render Controls @default 'around' */
+		controlsPosition?: CarouselControlsPosition;
+		/** Optional Carousel Item Wrapper @default `cardOutline` */
+		itemWrapper?: ItemWrapperOptions;
+		/** Number of Columns in Medium Viewport */
+		mdCols: MediumColumnOptions;
+		/** Number of Columns in Large Viewport */
+		lgCols?: LargeColumnOptions;
+	}>;
 
 export type CarouselState = Prettify<
-	Pick<CarouselProps, 'title' | 'mdCols'> &
+	Pick<CarouselProps, 'title' | 'subtitle' | 'mdCols'> &
 		Required<Pick<CarouselProps, 'lgCols'>> & {
 			items: Record<CarouselItemId, InternalCarouselItem>;
 			itemOrder: CarouselItemId[];
