7ba781c879ba328f702214e764251db48015d56f
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-14 22:28:32 -0600

    Sync title and items

--- src/components/content-display/gallery/gallery.tsx
+++ src/components/content-display/gallery/gallery.tsx
@@ -113,7 +113,7 @@ export const Gallery = ({
 	);
 
 	return (
-		<GalleryProvider initialValue={initialState}>
+		<GalleryProvider syncKeys={['title', 'subtitle', 'items']} initialValue={initialState}>
 			<GalleryEmblaProvider value={emblaValue}>
 				<GalleryContent />
 			</GalleryEmblaProvider>
