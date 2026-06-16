1f19d3f5054df2f66ecb63d70d655399bd9082de
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-15 23:35:43 -0600

    Standardize types

--- src/foundation/thread-provider/thread-provider.types.ts
+++ src/foundation/thread-provider/thread-provider.types.ts
@@ -1,15 +1,12 @@
 import { Prettify, ThemeConfig } from '@/types';
 import { ElementType, ReactNode } from 'react';
-import { ThemeProviderProps } from '../theme-provider/theme-provider.types';
 
-export type ThreadProviderProps = Prettify<
-	ThemeProviderProps &
-		ThreadFoundation & {
-			theme?: ThemeConfig;
-			children: ReactNode;
-		}
->;
+export type ThreadProviderProps = Prettify<{
+	linkComponent?: ThreadFoundation['LinkComponent'];
+	theme?: ThemeConfig;
+	children: ReactNode;
+}>;
 
 export type ThreadFoundation = {
-	linkComponent?: ElementType;
+	LinkComponent: ElementType;
 };
