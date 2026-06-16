320736ac16131a247817860e2885b1bd5648fb1c
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-15 20:25:38 -0600

    Remove provider exports

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,3 @@
 export { ThreadTheme } from './thread-theme';
-export { ThreadScript } from './theme-provider/thread-script';
-export { ThemeProvider, useThemeMode } from './theme-provider/theme-provider';
 export { SurfaceLayerMap, NextSurfaceLayerMap } from './theme-surface-system';
 export { TypographyRoles } from './theme-typography-system';
