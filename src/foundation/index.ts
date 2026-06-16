f418ca55f3993e3c48754cf2399552c383c2ece0
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-15 20:25:48 -0600

    Export theme providers

---
+++ src/foundation/index.ts
@@ -0,0 +1,3 @@
+// Theme Configuration
+export { ThreadScript } from '../foundation/theme-provider/thread-script';
+export { ThemeProvider, useThemeMode } from '../foundation/theme-provider/theme-provider';
