070afa6c2475347b67d37c56428c178eab8dd0e5
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-16 20:39:49 -0600

    Standardize Nav menu items

--- src/components/navigation/nav-menu/nav-menu.types.ts
+++ src/components/navigation/nav-menu/nav-menu.types.ts
@@ -1,10 +1,24 @@
-import { NavDropdownItemProps } from './components/nav-drop-down-item/nav-drop-down-item.types';
-import { NavigationLogoProps } from './components/nav-logo';
-import { NavItemProps } from './components/nav-item';
+import { ReactNode } from 'react';
+import { Prettify } from '@/types';
+import { NavigationItem } from '../navigation.types';
 
 export type NavMenuProps = {
 	/** Logo rendered as a linked icon on the left side of the nav */
 	logo?: NavigationLogoProps;
 	/** Navigation items. Accepts standard links or dropdown menus */
-	items: (NavItemProps | NavDropdownItemProps)[];
+	items: (NavigationItem | NavigationDropdownItem)[];
 };
+
+export type NavigationLogoProps = {
+	/** The URL or path the navigation item links to. */
+	href: NavigationItem['href'];
+	/** Site Logo */
+	logo: ReactNode;
+};
+
+export type NavigationDropdownItem = Prettify<
+	NavigationItem & {
+		/** Items within dropdown */
+		items: NavigationItem[];
+	}
+>;
