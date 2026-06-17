76eaa8279199bda10e35608d526717bdfb4de2fa
Andrew Fisher <fisherandrew777@gmail.com>
2026-06-16 22:20:17 -0600

    Display icon and text

--- src/components/navigation/nav-menu/components/nav-item.tsx
+++ src/components/navigation/nav-menu/components/nav-item.tsx
@@ -1,6 +1,15 @@
 import { NavLink } from './nav-link';
 import { NavigationItem } from '../../navigation.types';
+import { Text } from '@/components/typography';
+import { Icon } from '@/components/ui';
 
-export const NavItem = ({ href, title }: NavigationItem) => {
-	return <NavLink href={href}>{title}</NavLink>;
+export const NavItem = ({ href, title, icon }: NavigationItem) => {
+	return (
+		<NavLink href={href}>
+			{icon && <Icon color="text" name={icon} size={16} />}
+			<Text size="sm" inline weight="medium">
+				{title}
+			</Text>
+		</NavLink>
+	);
 };
