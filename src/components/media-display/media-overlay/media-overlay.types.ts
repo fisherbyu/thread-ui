4141cd8ec5e7edf2b399fcd5e3fc292e6fd2cebc
Andrew Fisher <fisherandrew777@gmail.com>
2026-07-01 21:30:19 -0600

    Add props

--- src/components/media-display/media-overlay/media-overlay.types.ts
+++ src/components/media-display/media-overlay/media-overlay.types.ts
@@ -1,9 +1,15 @@
 import { ScrimScaleOptions } from '@/types';
 import { ReactNode } from 'react';
 
+export type OverlayPlacementOptions = 'full' | 'top' | 'bottom' | 'left' | 'right';
+
 export type MediaOverlayProps = {
 	children: ReactNode;
 	overlay: ReactNode;
 	/** Scrim Background Opacity @default `medium` */
 	scrimLevel?: 'none' | ScrimScaleOptions;
+	/** Scrim Overlay Position @default `full` */
+	placement?: OverlayPlacementOptions;
+	maxHeight?: string;
+	maxWidth?: string;
 };
