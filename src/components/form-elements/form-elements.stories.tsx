c028b9f1515a5ba9a27f26d75b0f3b6e67efeccf
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-21 13:00:22 -0600

    Add size controls

--- src/components/form-elements/form-elements.stories.tsx
+++ src/components/form-elements/form-elements.stories.tsx
@@ -4,29 +4,48 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 import { TextInput } from './text-input';
 import { NumberInput } from './number-input';
 
+type CollectionArgs = {
+	size: 'sm' | 'md' | 'lg';
+};
+
 const meta = {
 	title: 'Form Elements/Collection',
 	tags: ['autodocs'],
-} satisfies Meta;
+	args: {
+		size: 'lg',
+	},
+	argTypes: {
+		size: {
+			control: 'radio',
+			options: ['sm', 'md', 'lg'],
+		},
+	},
+} satisfies Meta<CollectionArgs>;
 
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
 export const FormElements: Story = {
-	render: () => (
+	render: ({ size }) => (
 		<div
 			style={{
 				display: 'flex',
 				flexDirection: 'column',
 				margin: 'auto',
 				width: '75%',
-				gap: '16px',
+				// gap: '16px',
 			}}
 		>
-			<TextInput name="text" title="Full Name" placeholder="Enter your name" />
-			<TextInput name="notes" title="Notes" placeholder="Write something…" multiline />
-			<NumberInput name="count" title="Quantity" value={5} min={1} max={10} />
+			<TextInput name="text" title="Full Name" placeholder="Enter your name" size={size} />
+			<TextInput
+				name="notes"
+				title="Notes"
+				placeholder="Write something…"
+				multiline
+				size={size}
+			/>
+			<NumberInput name="count" title="Quantity" value={5} min={1} max={10} size={size} />
 		</div>
 	),
 };
