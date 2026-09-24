4dff9ddc7e1732a04260155eeddc3d3d731f1da7
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-24 00:08:07 -0600

    Derive prop type

--- src/components/form-elements/shared/form-label/form-label.types.ts
+++ src/components/form-elements/shared/form-label/form-label.types.ts
@@ -1,12 +1,15 @@
-import { UtilitySizeOptions } from '@/types';
+import { Override, Prettify } from '@/types';
+import { BaseInputProps } from '../input-props.types';
 
-export type FormLabelProps = {
-	/** Id of the control this label points to via `htmlFor`; the label's own id is `${id}-label` */
-	id: string;
-	/** Text displayed in the label */
-	title?: string;
-	/** Label Size @default `'md'`` */
-	size?: UtilitySizeOptions;
-	/** Optionally render divider beneath label @default false */
-	divider?: boolean;
-};
+export type FormLabelProps = Prettify<
+	Pick<
+		Override<
+			BaseInputProps,
+			{
+				/** Id of the control this label points to via `htmlFor`; the label's own id is `${id}-label` */
+				id: string;
+			}
+		>,
+		'id' | 'title' | 'size' | 'divider'
+	>
+>;
