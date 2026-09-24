d156a152ca622f55c8f8c07fe291b7fb3e1aad9a
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-24 01:34:42 -0600

    Update exports

--- src/components/form-elements/file-upload/index.ts
+++ src/components/form-elements/file-upload/index.ts
@@ -1,3 +1,9 @@
-export type { FileUploadProps, UploadableFile } from './file-upload.types';
+export type {
+	FileUploadProps,
+	UploadableFile,
+	RemoteFile,
+	FileUploadItem,
+	FileEditMode,
+} from './file-upload.types';
 export { FileUpload } from './file-upload';
 export { FilePreview, ImageDisplay } from './components/item-previews';
