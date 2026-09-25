2178969a0a5fefea72ab1ec784fd4a323763bde8
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-24 19:02:39 -0600

    Update props, drop inline and allow for as and margin rendering control

--- src/components/typography/code/code.types.ts
+++ src/components/typography/code/code.types.ts
@@ -3,10 +3,10 @@ import { TypographyProps } from '../typography.types';
 
 /** Code Component Props */
 export type CodeProps = Prettify<
-	Pick<TypographyProps, 'children' | 'inline' | 'truncate'> & {
+	Pick<TypographyProps, 'children' | 'truncate'> & {
 		/** Font size — body scale only @default `'sm'` */
 		size?: BodyFontSizeOptions;
-		/** Remove default background and coloring @default `true`` */
+		/** Remove default background and coloring @default `false` */
 		plain?: boolean;
 	}
 >;
