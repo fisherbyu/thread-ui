41e15f366eaf9b737e6b30f4d054c9cdb0002315
Andrew Fisher <fisherandrew777@gmail.com>
2026-09-26 02:59:53 -0600

    Export items

--- src/components/data-input/reorderable/reorderable-list/index.ts
+++ src/components/data-input/reorderable/reorderable-list/index.ts
@@ -1,8 +1,2 @@
 export type { ReorderableListProps } from './reorderable-list.types';
 export { ReorderableList } from './reorderable-list';
-export type {
-	ReorderableItem,
-	ReorderableItemChangeHandler,
-	ReorderableItemProps,
-	ReorderableItemComponent,
-} from './sortable-item';
